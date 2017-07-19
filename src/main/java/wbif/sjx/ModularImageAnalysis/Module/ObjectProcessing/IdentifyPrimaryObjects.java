1ba64ba01ee404c50a6f1de81b4a3ce4f1318977
Stephen Cross <stephen.cross@bristol.ac.uk>
2017-07-19 13:17:13 +0100

    Added 3D watershed to primary object identification
    Added 3D fill holes and distance map-based watershed to primary object identification.
    
    Fill holes can be done on a slice-by-slice basis or for the whole image stack in one go.

--- src/main/java/wbif/sjx/ModularImageAnalysis/Module/ObjectProcessing/IdentifyPrimaryObjects.java
+++ src/main/java/wbif/sjx/ModularImageAnalysis/Module/ObjectProcessing/IdentifyPrimaryObjects.java
@@ -6,8 +6,11 @@ import fiji.threshold.Auto_Threshold;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.Filters3D;
-import inra.ijpb.binary.conncomp.FloodFillComponentsLabeling3D;
+import inra.ijpb.binary.BinaryImages;
+import inra.ijpb.morphology.GeodesicReconstruction;
+import inra.ijpb.morphology.GeodesicReconstruction3D;
 import inra.ijpb.segment.Threshold;
+import inra.ijpb.watershed.ExtendedMinimaWatershed;
 import wbif.sjx.ModularImageAnalysis.Module.HCModule;
 import wbif.sjx.ModularImageAnalysis.Object.*;
 import wbif.sjx.ModularImageAnalysis.Object.ParameterCollection;
@@ -21,6 +24,12 @@ public class IdentifyPrimaryObjects extends HCModule {
     public static final String OUTPUT_OBJECT = "Output object";
     public static final String MEDIAN_FILTER_RADIUS = "Median filter radius";
     public static final String THRESHOLD_MULTIPLIER = "Threshold multiplier";
+    public static final String FILL_HOLES_BOOLEAN = "Fill holes";
+    public static final String FILL_HOLES_MODE = "Fill holes mode";
+
+    private static final String FILL_HOLES_3D = "Full 3D";
+    private static final String FILL_HOLES_SLICE = "Slice-by-slice in 2D";
+    private static final String[] FILL_HOLES_MODES = new String[]{FILL_HOLES_3D,FILL_HOLES_SLICE};
 
     @Override
     public String getTitle() {
@@ -41,6 +50,8 @@ public class IdentifyPrimaryObjects extends HCModule {
         double medFiltR = parameters.getValue(MEDIAN_FILTER_RADIUS);
         double thrMult = parameters.getValue(THRESHOLD_MULTIPLIER);
         String outputObjectName = parameters.getValue(OUTPUT_OBJECT);
+        boolean fillHoles = parameters.getValue(FILL_HOLES_BOOLEAN);
+        String fillHolesMode = parameters.getValue(FILL_HOLES_MODE);
 
         // Getting image stack
         String targetImageName = parameters.getValue(INPUT_IMAGE);
@@ -60,14 +71,45 @@ public class IdentifyPrimaryObjects extends HCModule {
         Object[] results1 = auto_threshold.exec(ipl,"Otsu",true,false,true,true,false,true);
         ipl = Threshold.threshold(ipl,(Integer) results1[0]*thrMult,Integer.MAX_VALUE);
 
-        // Applying connected components labelling
-        if (verbose) System.out.println("["+moduleName+"] Applying connected components labelling");
-        FloodFillComponentsLabeling3D ffcl3D = new FloodFillComponentsLabeling3D();
-        ipl.setStack(ffcl3D.computeLabels(ipl.getImageStack()));
+        // Applying fill holes 3D
+        if (fillHoles) {
+            switch (fillHolesMode) {
+                case FILL_HOLES_3D:
+                    if (verbose) System.out.println("[" + moduleName + "] Applying fill holes 3D");
+                    ipl.setStack(GeodesicReconstruction3D.fillHoles(ipl.getImageStack()));
+
+                    break;
+
+                case FILL_HOLES_SLICE:
+                    if (verbose) System.out.println("[" + moduleName + "] Applying fill holes slice-by-slice");
+                    for (int z = 0; z < ipl.getNSlices(); z++) {
+                        for (int c = 0; c < ipl.getNChannels(); c++) {
+                            for (int t = 0; t < ipl.getNFrames(); t++) {
+                                ipl.setPosition(c, z, t);
+
+                                ipl.setProcessor(GeodesicReconstruction.fillHoles(ipl.getProcessor()));
+
+                            }
+                        }
+                    }
+
+                    break;
+
+            }
+        }
+
+        // Calculating distance map
+        if (verbose) System.out.println("["+moduleName+"] Calculating distance map");
+        ImagePlus distIpl = new ImagePlus("Dist",BinaryImages.distanceMap(ipl.getImageStack()));
+        distIpl.getProcessor().invert();
+
+        // Calculating extended minima and maxima
+        if (verbose) System.out.println("["+moduleName+"] Applying watershed segmentation");
+        ipl.setStack(ExtendedMinimaWatershed.extendedMinimaWatershed(distIpl.getImageStack(),ipl.getImageStack(),1,6,false));
 
         // Converting image to objects
         if (verbose) System.out.println("["+moduleName+"] Converting image to objects");
-        Image tempImage = new Image(new String("Temp image"),ipl);
+        Image tempImage = new Image("Temp image",ipl);
         ObjSet outputObjects = new ObjectImageConverter().convertImageToObjects(tempImage,outputObjectName);
 
         // Adding objects to workspace
@@ -85,12 +127,28 @@ public class IdentifyPrimaryObjects extends HCModule {
         parameters.addParameter(new Parameter(OUTPUT_OBJECT, Parameter.OUTPUT_OBJECTS,null));
         parameters.addParameter(new Parameter(MEDIAN_FILTER_RADIUS, Parameter.DOUBLE,2.0));
         parameters.addParameter(new Parameter(THRESHOLD_MULTIPLIER, Parameter.DOUBLE,1.0));
+        parameters.addParameter(new Parameter(FILL_HOLES_BOOLEAN,Parameter.BOOLEAN,true));
+        parameters.addParameter(new Parameter(FILL_HOLES_MODE,Parameter.CHOICE_ARRAY,FILL_HOLES_MODES[0],FILL_HOLES_MODES));
 
     }
 
     @Override
     public ParameterCollection getActiveParameters() {
-        return parameters;
+        ParameterCollection returnedParameters = new ParameterCollection();
+
+        returnedParameters.addParameter(parameters.getParameter(INPUT_IMAGE));
+        returnedParameters.addParameter(parameters.getParameter(OUTPUT_OBJECT));
+        returnedParameters.addParameter(parameters.getParameter(MEDIAN_FILTER_RADIUS));
+        returnedParameters.addParameter(parameters.getParameter(THRESHOLD_MULTIPLIER));
+        returnedParameters.addParameter(parameters.getParameter(FILL_HOLES_BOOLEAN));
+
+        if (parameters.getValue(FILL_HOLES_BOOLEAN)) {
+            returnedParameters.addParameter(parameters.getParameter(FILL_HOLES_MODE));
+
+        }
+
+        return returnedParameters;
+
     }
 
     @Override
