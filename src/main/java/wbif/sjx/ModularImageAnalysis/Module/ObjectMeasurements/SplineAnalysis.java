99489a00adc268565f1eebd466f248b980861097
Stephen Cross <stephen.cross@bristol.ac.uk>
2018-01-29 09:49:55 +0000

    MInor updates to spline analysis
    Implemented new curvature calculator from the Common library.

--- src/main/java/wbif/sjx/ModularImageAnalysis/Module/ObjectMeasurements/SplineAnalysis.java
+++ src/main/java/wbif/sjx/ModularImageAnalysis/Module/ObjectMeasurements/SplineAnalysis.java
@@ -1,6 +1,7 @@
 package wbif.sjx.ModularImageAnalysis.Module.ObjectMeasurements;
 
 import ij.ImagePlus;
+import ij.plugin.Duplicator;
 import org.apache.commons.math3.analysis.interpolation.DividedDifferenceInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionNewtonForm;
@@ -12,6 +13,7 @@ import wbif.sjx.ModularImageAnalysis.Module.ImageProcessing.BinaryOperations;
 import wbif.sjx.ModularImageAnalysis.Module.ImageProcessing.InvertIntensity;
 import wbif.sjx.ModularImageAnalysis.Module.ObjectProcessing.ObjectImageConverter;
 import wbif.sjx.ModularImageAnalysis.Object.*;
+import wbif.sjx.common.Analysis.CurvatureCalculator;
 import wbif.sjx.common.Object.Vertex;
 import wbif.sjx.common.Process.SkeletonTools.Skeleton;
 import wbif.sjx.common.Process.SkeletonTools.SkeletonVisualiser;
@@ -24,6 +26,7 @@ import java.util.LinkedHashSet;
  */
 public class SplineAnalysis extends HCModule {
     public static final String INPUT_OBJECTS = "Input objects";
+    public static final String INPUT_IMAGE = "Input image";
 
     @Override
     public String getTitle() {
@@ -41,27 +44,41 @@ public class SplineAnalysis extends HCModule {
         String inputObjectName = parameters.getValue(INPUT_OBJECTS);
         ObjCollection inputObjects = workspace.getObjects().get(inputObjectName);
 
+        String inputImageName = parameters.getValue(INPUT_IMAGE);
+        Image inputImage = workspace.getImage(inputImageName);
+        ImagePlus inputImagePlus = inputImage.getImagePlus();
+
         // Converting object to image, then inverting, so we have a black object on a white background
-        ImagePlus objectIpl = inputObjects.values().iterator().next().getAsImage("Object").getImagePlus();
+//        ImagePlus objectIpl = inputObjects.values().iterator().next().getAsImage("Object").getImagePlus();
+        ObjCollection tempObj = new ObjCollection("Backbone");
+        ImagePlus objectIpl = ObjectImageConverter.convertObjectsToImage(tempObj,"Temp",inputImagePlus,ObjectImageConverter.ColourModes.SINGLE_COLOUR,"",false).getImagePlus();
+        
 
         InvertIntensity.process(objectIpl);
 
         // Skeletonise fish to get single backbone
         BinaryOperations.applyBinaryTransform(objectIpl,BinaryOperations.OperationModes.SKELETONISE_2D,1,0);
-        objectIpl.show();
 
         // Using the Common library's Skeleton tools to extract the longest branch.  This requires coordinates for the
         Skeleton skeleton = new Skeleton(objectIpl);
         LinkedHashSet<Vertex> longestPath = skeleton.getLongestPath();
 
         // Calculating local curvature along the path
-        
+        CurvatureCalculator curvatureCalculator = new CurvatureCalculator(longestPath);
+
+        // Displaying the image (the image is duplicated, so it doesn't get deleted if the window is closed)
+//        if (showImage && ipl != null) {
+        inputImagePlus = new Duplicator().run(inputImagePlus);
+        curvatureCalculator.showOverlay(inputImagePlus);
+        inputImagePlus.show();
+//        }
 
     }
 
     @Override
     protected void initialiseParameters() {
         parameters.add(new Parameter(INPUT_OBJECTS, Parameter.INPUT_OBJECTS,null));
+        parameters.add(new Parameter(INPUT_IMAGE, Parameter.INPUT_IMAGE,null));
 
     }
 
