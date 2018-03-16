a18242cea1f9f8e2ad800633d4213fe8e8abfd1a
Stephen Cross <stephen.cross@bristol.ac.uk>
2018-03-16 09:45:08 +0000

    Tests for Image and Measurements
    Basic tests on constructors and other methods.

--- src/test/java/wbif/sjx/ModularImageAnalysis/Module/InputOutput/ImageSaverTest.java
+++ src/test/java/wbif/sjx/ModularImageAnalysis/Module/InputOutput/ImageSaverTest.java
@@ -1,7 +1,19 @@
 package wbif.sjx.ModularImageAnalysis.Module.InputOutput;
 
+import ij.IJ;
+import ij.ImagePlus;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import wbif.sjx.ModularImageAnalysis.ExpectedObjects3D;
+import wbif.sjx.ModularImageAnalysis.ExpectedSpots3D;
+import wbif.sjx.ModularImageAnalysis.Module.ObjectProcessing.Refinement.MergeObjects;
+import wbif.sjx.ModularImageAnalysis.Object.Image;
+import wbif.sjx.ModularImageAnalysis.Object.ObjCollection;
+import wbif.sjx.ModularImageAnalysis.Object.Workspace;
+
+import java.io.File;
+import java.net.URLDecoder;
 
 import static org.junit.Assert.*;
 
@@ -14,16 +26,89 @@ public class ImageSaverTest {
         assertNotNull(new ImageSaver().getTitle());
     }
 
-    @Test @Ignore
+    @Test
     public void testRunSaveWithInputFile() throws Exception {
+        // Create a temporary folder and tell the workspace that's where the input was (even though it wasn't)
+        TemporaryFolder temporaryFolder = new TemporaryFolder();
+        temporaryFolder.create();
+        File testFile = temporaryFolder.newFile("TestFile.tif");
+
+        // Creating a new workspace
+        Workspace workspace = new Workspace(0,testFile);
+
+        // Load the test image and put in the workspace
+        String pathToImage = URLDecoder.decode(this.getClass().getResource("/images/BinaryObjects2D_8bit_whiteBG.tif").getPath(),"UTF-8");
+        ImagePlus ipl = IJ.openImage(pathToImage);
+        Image image = new Image("Test_image",ipl);
+        workspace.addImage(image);
+
+        // Initialising FilterObjects module
+        ImageSaver imageSaver = new ImageSaver();
+        imageSaver.initialiseParameters();
+        imageSaver.updateParameterValue(ImageSaver.INPUT_IMAGE,"Test_image");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_LOCATION,ImageSaver.SaveLocations.SAVE_WITH_INPUT);
+        imageSaver.updateParameterValue(ImageSaver.MIRROR_DIRECTORY_ROOT,"");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_FILE_PATH,"");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_SUFFIX,"_test");
+        imageSaver.updateParameterValue(ImageSaver.FLATTEN_OVERLAY,false);
+
+        // Running the module
+        imageSaver.run(workspace,false);
+
+        // Checking the new file exists in the temporary folder
+        String[] tempFileContents = temporaryFolder.getRoot().list();
+        boolean contains = false;
+        for (String name:tempFileContents) {
+            if (name.equals("TestFile_test.tif")) {
+                contains = true;
+            }
+        }
+        assertTrue(contains);
+
     }
 
-    @Test @Ignore
+    @Test
     public void testRunSaveAtSpecificLocation() throws Exception {
+        // Create a temporary folder and tell the workspace that's where the input was (even though it wasn't)
+        TemporaryFolder temporaryFolder = new TemporaryFolder();
+        temporaryFolder.create();
+
+        // Creating a new workspace
+        Workspace workspace = new Workspace(0,null);
+
+        // Load the test image and put in the workspace
+        String pathToImage = URLDecoder.decode(this.getClass().getResource("/images/BinaryObjects2D_8bit_whiteBG.tif").getPath(),"UTF-8");
+        ImagePlus ipl = IJ.openImage(pathToImage);
+        Image image = new Image("Test_image",ipl);
+        workspace.addImage(image);
+
+        // Initialising FilterObjects module
+        ImageSaver imageSaver = new ImageSaver();
+        imageSaver.initialiseParameters();
+        imageSaver.updateParameterValue(ImageSaver.INPUT_IMAGE,"Test_image");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_LOCATION,ImageSaver.SaveLocations.SPECIFIC_LOCATION);
+        imageSaver.updateParameterValue(ImageSaver.MIRROR_DIRECTORY_ROOT,"");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_FILE_PATH,temporaryFolder.getRoot().getAbsolutePath()+"\\TestFile.tif");
+        imageSaver.updateParameterValue(ImageSaver.SAVE_SUFFIX,"_test2");
+        imageSaver.updateParameterValue(ImageSaver.FLATTEN_OVERLAY,false);
+
+        // Running the module
+        imageSaver.run(workspace,false);
+
+        // Checking the new file exists in the temporary folder
+        String[] tempFileContents = temporaryFolder.getRoot().list();
+        boolean contains = false;
+        for (String name:tempFileContents) {
+            if (name.equals("TestFile_test2.tif")) {
+                contains = true;
+            }
+        }
+        assertTrue(contains);
     }
 
     @Test @Ignore
     public void testRunSaveInMirroredDirectory() throws Exception {
+
     }
 
     @Test @Ignore
---
+++ src/test/java/wbif/sjx/ModularImageAnalysis/Object/MeasurementReferenceCollectionTest.java
@@ -0,0 +1,66 @@
+package wbif.sjx.ModularImageAnalysis.Object;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MeasurementReferenceCollectionTest {
+    @Test
+    public void get() {
+        // Creating some measurements
+        MeasurementReference ref1 = new MeasurementReference("Ref 1");
+        MeasurementReference ref2 = new MeasurementReference("Second ref");
+        MeasurementReference ref3 = new MeasurementReference("Ref 3");
+        MeasurementReference ref4 = new MeasurementReference("One more");
+
+        // Populating the collection
+        MeasurementReferenceCollection collection = new MeasurementReferenceCollection();
+        collection.add(ref1);
+        collection.add(ref2);
+        collection.add(ref3);
+        collection.add(ref4);
+
+        assertEquals(ref1,collection.get("Ref 1"));
+        assertEquals(ref3,collection.get("Ref 3"));
+        assertEquals(ref2,collection.get("Second ref"));
+        assertEquals(ref4,collection.get("One more"));
+
+        assertNull(collection.get("Not there"));
+        assertNull(collection.get("Ref 11"));
+
+    }
+
+    @Test
+    public void updateImageObjectName() {
+        // Creating some measurements
+        MeasurementReference ref1 = new MeasurementReference("Ref 1");
+        MeasurementReference ref2 = new MeasurementReference("Second ref");
+        MeasurementReference ref3 = new MeasurementReference("Ref 3");
+        MeasurementReference ref4 = new MeasurementReference("One more");
+
+        // Populating the collection
+        MeasurementReferenceCollection collection = new MeasurementReferenceCollection();
+        collection.add(ref1);
+        collection.add(ref2);
+        collection.add(ref3);
+        collection.add(ref4);
+
+        // Checking the imageObjectName before changing
+        assertEquals("",collection.get("Second ref").getImageObjName());
+
+        // Changing the name
+        collection.updateImageObjectName("Second ref","im name");
+
+        // Checking the imageObjectName values for all references
+        assertEquals("",collection.get("Ref 1").getImageObjName());
+        assertEquals("im name",collection.get("Second ref").getImageObjName());
+        assertEquals("",collection.get("Ref 3").getImageObjName());
+        assertEquals("",collection.get("One more").getImageObjName());
+
+    }
+
+    @Test @Ignore
+    public void getMeasurementNickNames() {
+    }
+}
