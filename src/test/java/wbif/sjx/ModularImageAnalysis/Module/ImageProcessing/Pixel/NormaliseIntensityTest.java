e16d0edc0f36312e8ecf6c3cd4b8c9e0894b4382
Stephen Cross <stephen.cross@bristol.ac.uk>
2018-05-29 09:40:12 +0100

    Added more NormaliseIntensity unit tests
    Corrected issue with NormaliseIntensity, which caused 5D stacks to be processed incorrectly.

--- src/test/java/wbif/sjx/ModularImageAnalysis/Module/ImageProcessing/Pixel/NormaliseIntensityTest.java
+++ src/test/java/wbif/sjx/ModularImageAnalysis/Module/ImageProcessing/Pixel/NormaliseIntensityTest.java
@@ -81,7 +81,7 @@ public class NormaliseIntensityTest {
         }
     }
 
-    @Test @Ignore
+    @Test
     public void testNormaliseIntensity8bit3D() throws Exception {
         // Creating a new workspace
         Workspace workspace = new Workspace(0,null,1);
@@ -144,12 +144,130 @@ public class NormaliseIntensityTest {
         }
     }
 
-    @Test @Ignore
-    public void testNormaliseIntensity8bit4D() {
+    @Test
+    public void testNormaliseIntensity8bit4D() throws Exception {
+        // Creating a new workspace
+        Workspace workspace = new Workspace(0,null,1);
+
+        // Setting calibration parameters
+        double dppXY = 0.02;
+        String calibratedUnits = "µm";
+
+        // Loading the test image and adding to workspace
+        String pathToImage = URLDecoder.decode(this.getClass().getResource("/images/NormaliseIntensity/DarkNoisyGradient5D_8bit_C1.tif").getPath(),"UTF-8");
+        ImagePlus ipl = IJ.openImage(pathToImage);
+        Image image = new Image("Test_image",ipl);
+        workspace.addImage(image);
+
+        pathToImage = URLDecoder.decode(this.getClass().getResource("/images/NormaliseIntensity/DarkNoisyGradientNormalised5D_8bit_C1.tif").getPath(),"UTF-8");
+        ImagePlus expectedImage = IJ.openImage(pathToImage);
+
+        // Initialising BinaryOperations
+        NormaliseIntensity normaliseIntensity = new NormaliseIntensity();
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.INPUT_IMAGE,"Test_image");
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.APPLY_TO_INPUT,false);
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.OUTPUT_IMAGE,"Test_output");
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.SHOW_IMAGE,false);
+
+        // Running NormaliseIntensity
+        normaliseIntensity.run(workspace);
+
+        // Checking the images in the workspace
+        assertEquals(2,workspace.getImages().size());
+        assertNotNull(workspace.getImage("Test_image"));
+        assertNotNull(workspace.getImage("Test_output"));
+
+        // Checking the output image has the expected calibration
+        ImagePlus outputImage = workspace.getImage("Test_output").getImagePlus();
+        assertEquals(dppXY,outputImage.getCalibration().pixelWidth,1E-2);
+        assertEquals(calibratedUnits,outputImage.getCalibration().getXUnit());
+        assertEquals(8,outputImage.getBitDepth());
+
+        // Checking the size of the output image
+        assertEquals(64,outputImage.getWidth());
+        assertEquals(76,outputImage.getHeight());
+        assertEquals(1,outputImage.getNChannels());
+        assertEquals(12,outputImage.getNSlices());
+        assertEquals(4,outputImage.getNFrames());
+
+        // Checking the individual image pixel values
+        for (int c=0;c<outputImage.getNChannels();c++) {
+            for (int z = 0; z < outputImage.getNSlices(); z++) {
+                for (int t = 0; t < outputImage.getNFrames(); t++) {
+                    expectedImage.setPosition(c+1, z + 1, t + 1);
+                    outputImage.setPosition(c+1, z + 1, t + 1);
+
+                    float[][] expectedValues = expectedImage.getProcessor().getFloatArray();
+                    float[][] actualValues = outputImage.getProcessor().getFloatArray();
+
+                    assertArrayEquals(expectedValues, actualValues);
+
+                }
+            }
+        }
     }
 
-    @Test @Ignore
-    public void testNormaliseIntensity8bit5D() {
+    @Test
+    public void testNormaliseIntensity8bit5D() throws Exception {
+        // Creating a new workspace
+        Workspace workspace = new Workspace(0,null,1);
+
+        // Setting calibration parameters
+        double dppXY = 0.02;
+        String calibratedUnits = "µm";
+
+        // Loading the test image and adding to workspace
+        String pathToImage = URLDecoder.decode(this.getClass().getResource("/images/NormaliseIntensity/DarkNoisyGradient5D_8bit.tif").getPath(),"UTF-8");
+        ImagePlus ipl = IJ.openImage(pathToImage);
+        Image image = new Image("Test_image",ipl);
+        workspace.addImage(image);
+
+        pathToImage = URLDecoder.decode(this.getClass().getResource("/images/NormaliseIntensity/DarkNoisyGradientNormalised5D_8bit.tif").getPath(),"UTF-8");
+        ImagePlus expectedImage = IJ.openImage(pathToImage);
+
+        // Initialising BinaryOperations
+        NormaliseIntensity normaliseIntensity = new NormaliseIntensity();
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.INPUT_IMAGE,"Test_image");
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.APPLY_TO_INPUT,false);
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.OUTPUT_IMAGE,"Test_output");
+        normaliseIntensity.updateParameterValue(NormaliseIntensity.SHOW_IMAGE,false);
+
+        // Running NormaliseIntensity
+        normaliseIntensity.run(workspace);
+
+        // Checking the images in the workspace
+        assertEquals(2,workspace.getImages().size());
+        assertNotNull(workspace.getImage("Test_image"));
+        assertNotNull(workspace.getImage("Test_output"));
+
+        // Checking the output image has the expected calibration
+        ImagePlus outputImage = workspace.getImage("Test_output").getImagePlus();
+        assertEquals(dppXY,outputImage.getCalibration().pixelWidth,1E-2);
+        assertEquals(calibratedUnits,outputImage.getCalibration().getXUnit());
+        assertEquals(8,outputImage.getBitDepth());
+
+        // Checking the size of the output image
+        assertEquals(64,outputImage.getWidth());
+        assertEquals(76,outputImage.getHeight());
+        assertEquals(2,outputImage.getNChannels());
+        assertEquals(12,outputImage.getNSlices());
+        assertEquals(4,outputImage.getNFrames());
+
+        // Checking the individual image pixel values
+        for (int c=0;c<outputImage.getNChannels();c++) {
+            for (int z = 0; z < outputImage.getNSlices(); z++) {
+                for (int t = 0; t < outputImage.getNFrames(); t++) {
+                    expectedImage.setPosition(c+1, z + 1, t + 1);
+                    outputImage.setPosition(c+1, z + 1, t + 1);
+
+                    float[][] expectedValues = expectedImage.getProcessor().getFloatArray();
+                    float[][] actualValues = outputImage.getProcessor().getFloatArray();
+
+                    assertArrayEquals(expectedValues, actualValues);
+
+                }
+            }
+        }
     }
 
     @Test @Ignore
@@ -163,4 +281,5 @@ public class NormaliseIntensityTest {
     @Test @Ignore
     public void testRunDoApply8bit() {
     }
+
 }
