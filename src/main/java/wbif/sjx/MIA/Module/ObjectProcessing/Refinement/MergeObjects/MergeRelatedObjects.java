90c3ebbbb7fe28131e3ebdf612381f691bdf6feb
Stephen Cross <stephencross86@gmail.com>
2019-07-12 09:55:18 +0100

    Bugfixes for tests
    Not all modules had kept track of RelateObjects moving.

--- src/main/java/wbif/sjx/MIA/Module/ObjectProcessing/Refinement/MergeObjects/MergeRelatedObjects.java
+++ src/main/java/wbif/sjx/MIA/Module/ObjectProcessing/Refinement/MergeObjects/MergeRelatedObjects.java
@@ -3,7 +3,6 @@ package wbif.sjx.MIA.Module.ObjectProcessing.Refinement.MergeObjects;
 import ij.ImagePlus;
 import wbif.sjx.MIA.Module.Module;
 import wbif.sjx.MIA.Module.ModuleCollection;
-import wbif.sjx.MIA.Module.ObjectMeasurements.Spatial.RelateObjects;
 import wbif.sjx.MIA.Module.PackageNames;
 import wbif.sjx.MIA.Object.Obj;
 import wbif.sjx.MIA.Object.ObjCollection;
