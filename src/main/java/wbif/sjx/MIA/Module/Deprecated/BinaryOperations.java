adff0d695e864e72fc7189d3d177f092e8b6d117
Stephen Cross <stephencross86@gmail.com>
2019-07-23 13:33:53 +0100

    Bugfix for 3D binary operations
    Structure element for 3D binary operations was incorrectly set to be elongated along X.

--- src/main/java/wbif/sjx/MIA/Module/Deprecated/BinaryOperations.java
+++ src/main/java/wbif/sjx/MIA/Module/Deprecated/BinaryOperations.java
@@ -127,7 +127,7 @@ public class BinaryOperations extends Module {
         double dppZ = ipl.getCalibration().pixelDepth;
         double ratio = dppXY/dppZ;
 
-        Strel3D ballStrel = Strel3D.Shape.BALL.fromRadiusList(numIterations,(int) (numIterations*ratio),2);
+        Strel3D ballStrel = Strel3D.Shape.BALL.fromRadiusList(numIterations,numIterations,(int) (numIterations*ratio));
 
         // MorphoLibJ takes objects as being white
         InvertIntensity.process(ipl);
