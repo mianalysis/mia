c40332991c8b37352100620ddee926c69c72fada
Stephen Cross <stephen.cross@bristol.ac.uk>
2019-10-04 21:37:49 +0100

    Improved date handling
    Updates to loading based on date.

--- src/main/java/wbif/sjx/MIA/GUI/Splash.java
+++ src/main/java/wbif/sjx/MIA/GUI/Splash.java
@@ -1,8 +1,13 @@
 package wbif.sjx.MIA.GUI;
 
+import ij.IJ;
+import wbif.sjx.MIA.MIA;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Splash extends JFrame {
@@ -10,16 +15,27 @@ public class Splash extends JFrame {
     private int width = 231;
     private JLabel image;
 
+    private final static String DETECTING_MODULES = "/Images/Logo_splash_detecting-modules";
+    private final static String INITIALISING_MODULES = "/Images/Logo_splash_initialising-modules";
+    private final static String CREATING_INTERFACE = "/Images/Logo_splash_creating-interface";
+
     private ImageIcon blankLogo = new ImageIcon(Splash.class.getResource("/Images/Logo_splash.png"));
-    private ImageIcon detectingModulesLogo = new ImageIcon(Splash.class.getResource("/Images/Logo_splash_detecting-modules.png"));
-    private ImageIcon initialisingModulesLogo = new ImageIcon(Splash.class.getResource("/Images/Logo_splash_initialising-modules.png"));
-    private ImageIcon creatingInterfaceLogo = new ImageIcon(Splash.class.getResource("/Images/Logo_splash_creating-interface.png"));
+    private final ImageIcon detectingModulesLogo;
+    private final ImageIcon initialisingModulesLogo;
+    private final ImageIcon creatingInterfaceLogo;
 
     public enum Status {
         BLANK, DETECTING_MODULES, INITIALISING_MODULES, CREATING_INTERFACE;
     }
 
     public Splash() {
+        // Determine special date
+        String suffix = getSpecialSuffix();
+
+        detectingModulesLogo = new ImageIcon(Splash.class.getResource(DETECTING_MODULES+suffix+".png"));
+        initialisingModulesLogo = new ImageIcon(Splash.class.getResource(INITIALISING_MODULES+suffix+".png"));
+        creatingInterfaceLogo = new ImageIcon(Splash.class.getResource(CREATING_INTERFACE+suffix+".png"));
+
         setUndecorated(true);
         setBackground(new Color(0,0,0,0));
         setAlwaysOnTop(true);
@@ -50,5 +66,15 @@ public class Splash extends JFrame {
                 image.setIcon(creatingInterfaceLogo);
                 break;
         }
+    }
+
+    static String getSpecialSuffix() {
+        switch (new SimpleDateFormat("dd-MM").format(new Date())) {
+            case "31-10":
+                return "_3110";
+        }
+
+        return "";
+
     }
 }
