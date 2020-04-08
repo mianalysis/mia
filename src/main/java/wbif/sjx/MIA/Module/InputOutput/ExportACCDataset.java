944013e6ab0fc207b1e34a011eecb90b8ea1cff5
Stephen Cross <stephencross86@gmail.com>
2020-04-08 09:20:04 +0100

    Bugfix for ACC exporter if seriesname includes slashes
    Slashes in series name are replaced with underscores.

--- src/main/java/wbif/sjx/MIA/Module/InputOutput/ExportACCDataset.java
+++ src/main/java/wbif/sjx/MIA/Module/InputOutput/ExportACCDataset.java
@@ -44,24 +44,17 @@ public class ExportACCDataset extends Module {
     }
 
     static String getFolderName(String rootFolder, String analysisFolderName, String plateName) {
-        return rootFolder+"\\"+plateName+"\\"+analysisFolderName+"\\";
+        return rootFolder + "\\" + plateName + "\\" + analysisFolderName + "\\";
 
     }
 
-    static String getFileName(String plateName, String rowLetter, int columnNumber, int seriesNumber, String seriesName, String extension) {
+    static String getFileName(String plateName, String rowLetter, int columnNumber, int seriesNumber, String seriesName,
+            String extension) {
         DecimalFormat df = new DecimalFormat("00");
         StringBuilder builder = new StringBuilder();
 
-        builder.append(plateName)
-                .append("_w")
-                .append(rowLetter)
-                .append(df.format(columnNumber))
-                .append("_s")
-                .append(seriesNumber)
-                .append("_n")
-                .append(seriesName)
-                .append(".")
-                .append(extension);
+        builder.append(plateName).append("_w").append(rowLetter).append(df.format(columnNumber)).append("_s")
+                .append(seriesNumber).append("_n").append(seriesName).append(".").append(extension);
 
         return builder.toString();
 
@@ -82,16 +75,18 @@ public class ExportACCDataset extends Module {
         ImagePlus rgbImage = ipl.flatten();
 
         // Writing image to file (using png to keep data size down)
-        IJ.saveAs(rgbImage,"PNG",folderName+fileName);
+        IJ.saveAs(rgbImage, "PNG", folderName + fileName);
 
         return true;
 
     }
 
-    static boolean saveFeatureNames(ObjMeasurementRefCollection refs, TreeMap<String,Boolean> states, String folderName) {
+    static boolean saveFeatureNames(ObjMeasurementRefCollection refs, TreeMap<String, Boolean> states,
+            String folderName) {
         // Check if featureName file exists
-        File featureNamesFile = new File(folderName+"featureNames.acc");
-        if (featureNamesFile.exists()) return true;
+        File featureNamesFile = new File(folderName + "featureNames.acc");
+        if (featureNamesFile.exists())
+            return true;
 
         // Check analysis folder exists
         File featureFolder = new File(folderName);
@@ -103,14 +98,14 @@ public class ExportACCDataset extends Module {
 
         // Compiling list of measurement names
         StringBuilder builder = new StringBuilder();
-        builder.append("Location_Center_X\n")
-                .append("Location_Center_Y\n");
-        for (ObjMeasurementRef ref:refs.values()) {
-            if (!states.containsKey(ref.getName())) continue;
-            if (!states.get(ref.getName())) continue;
+        builder.append("Location_Center_X\n").append("Location_Center_Y\n");
+        for (ObjMeasurementRef ref : refs.values()) {
+            if (!states.containsKey(ref.getName()))
+                continue;
+            if (!states.get(ref.getName()))
+                continue;
 
-            builder.append(ref.getFinalName().replace(" ","_"))
-                    .append("\n");
+            builder.append(ref.getFinalName().replace(" ", "_")).append("\n");
         }
 
         // Writing the feature names to file
@@ -126,7 +121,8 @@ public class ExportACCDataset extends Module {
 
     }
 
-    static boolean saveFeatures(ObjCollection objects, ObjMeasurementRefCollection refs, TreeMap<String,Boolean> states, String folderName, String fileName) {
+    static boolean saveFeatures(ObjCollection objects, ObjMeasurementRefCollection refs,
+            TreeMap<String, Boolean> states, String folderName, String fileName) {
         DecimalFormat df = new DecimalFormat("0.0000000E0");
 
         // Check analysis folder exists
@@ -139,19 +135,18 @@ public class ExportACCDataset extends Module {
 
         StringBuilder builder = new StringBuilder();
 
-        for (Obj obj:objects.values()) {
+        for (Obj obj : objects.values()) {
             // Adding centroid
-            builder.append(df.format(obj.getXMean(true)))
-                    .append(" ")
-                    .append(df.format(obj.getYMean(true)));
+            builder.append(df.format(obj.getXMean(true))).append(" ").append(df.format(obj.getYMean(true)));
 
             // Adding extra features
-            for (ObjMeasurementRef ref:refs.values()) {
-                if (!states.containsKey(ref.getName())) continue;
-                if (!states.get(ref.getName())) continue;
+            for (ObjMeasurementRef ref : refs.values()) {
+                if (!states.containsKey(ref.getName()))
+                    continue;
+                if (!states.get(ref.getName()))
+                    continue;
 
-                builder.append(" ")
-                        .append(df.format(obj.getMeasurement(ref.getName()).getValue()));
+                builder.append(" ").append(df.format(obj.getMeasurement(ref.getName()).getValue()));
             }
 
             builder.append("\n");
@@ -160,7 +155,7 @@ public class ExportACCDataset extends Module {
 
         // Writing the features to file
         try {
-            File featuresFile = new File(folderName+fileName);
+            File featuresFile = new File(folderName + fileName);
             FileWriter writer = new FileWriter(featuresFile);
             writer.write(builder.toString());
             writer.close();
@@ -187,36 +182,40 @@ public class ExportACCDataset extends Module {
         String rowLetter = workspace.getMetadata().getAsString(rowMetadataName);
         String columnMetadataName = parameters.getValue(COLUMN_NUMBER);
         int columnNumber = Integer.parseInt(workspace.getMetadata().getAsString(columnMetadataName));
-        TreeMap<String,Boolean> states = parameters.getValue(MEASUREMENTS);
+        TreeMap<String, Boolean> states = parameters.getValue(MEASUREMENTS);
 
-        // Constructing filename
+        // Constructing filename (also removing any slash characters from seriesName)
         int seriesNumber = workspace.getMetadata().getSeriesNumber();
         String seriesName = workspace.getMetadata().getSeriesName();
-        String imageFilename = getFileName(plateName,rowLetter,columnNumber,seriesNumber,seriesName,"png");
-        String textFilename = getFileName(plateName,rowLetter,columnNumber,seriesNumber,seriesName,"txt");
+        seriesName = seriesName.replace("\\", "_");
+        seriesName = seriesName.replace("/", "_");
+        String imageFilename = getFileName(plateName, rowLetter, columnNumber, seriesNumber, seriesName, "png");
+        String textFilename = getFileName(plateName, rowLetter, columnNumber, seriesNumber, seriesName, "txt");
+
+
 
         // Storing raw image
-        String folderName1 = getFolderName(rootFolder,"anal1",plateName);
-        if (!saveImage(inputOverlayImage,folderName1,imageFilename)) {
+        String folderName1 = getFolderName(rootFolder, "anal1", plateName);
+        if (!saveImage(inputOverlayImage, folderName1, imageFilename)) {
             MIA.log.writeWarning("Could not write overlay image to file");
             return false;
         }
-        String folderName3 = getFolderName(rootFolder,"anal3",plateName);
-        if (!saveImage(inputRawImage,folderName3,imageFilename)) {
+        String folderName3 = getFolderName(rootFolder, "anal3", plateName);
+        if (!saveImage(inputRawImage, folderName3, imageFilename)) {
             MIA.log.writeWarning("Could not write raw image to file");
             return false;
         }
 
         // Getting measurements for input objects
-        String folderName2 = getFolderName(rootFolder,"anal2",plateName);
+        String folderName2 = getFolderName(rootFolder, "anal2", plateName);
         ObjMeasurementRefCollection measurementRefs = modules.getObjectMeasurementRefs(inputObjectsName);
-        if (!saveFeatureNames(measurementRefs,states,folderName2)) {
+        if (!saveFeatureNames(measurementRefs, states, folderName2)) {
             MIA.log.writeWarning("Could not write feature names to file");
             return false;
         }
 
         // Writing object features to file
-        if (!saveFeatures(inputObjects,measurementRefs,states,folderName2,textFilename)) {
+        if (!saveFeatures(inputObjects, measurementRefs, states, folderName2, textFilename)) {
             MIA.log.writeWarning("Could not write features to file");
             return false;
         }
@@ -227,18 +226,18 @@ public class ExportACCDataset extends Module {
 
     @Override
     protected void initialiseParameters() {
-        parameters.add(new ParamSeparatorP(INPUT_SEPARATOR,this));
-        parameters.add(new InputObjectsP(INPUT_OBJECTS,this));
-        parameters.add(new InputImageP(INPUT_RAW_IMAGE,this));
-        parameters.add(new InputImageP(INPUT_OVERLAY_IMAGE,this));
-        parameters.add(new ParamSeparatorP(OUTPUT_SEPARATOR,this));
-        parameters.add(new FolderPathP(ROOT_DATASET_FOLDER,this));
-        parameters.add(new MetadataItemP(PLATE_NAME,this));
-        parameters.add(new MetadataItemP(ROW_LETTER,this));
-        parameters.add(new MetadataItemP(COLUMN_NUMBER,this));
-        parameters.add(new ParamSeparatorP(MEASUREMENT_SEPARATOR,this));
-        parameters.add(new BooleanP(SHOW_MEASUREMENTS,this,true));
-        parameters.add(new ObjMeasurementSelectorP(MEASUREMENTS,this));
+        parameters.add(new ParamSeparatorP(INPUT_SEPARATOR, this));
+        parameters.add(new InputObjectsP(INPUT_OBJECTS, this));
+        parameters.add(new InputImageP(INPUT_RAW_IMAGE, this));
+        parameters.add(new InputImageP(INPUT_OVERLAY_IMAGE, this));
+        parameters.add(new ParamSeparatorP(OUTPUT_SEPARATOR, this));
+        parameters.add(new FolderPathP(ROOT_DATASET_FOLDER, this));
+        parameters.add(new MetadataItemP(PLATE_NAME, this));
+        parameters.add(new MetadataItemP(ROW_LETTER, this));
+        parameters.add(new MetadataItemP(COLUMN_NUMBER, this));
+        parameters.add(new ParamSeparatorP(MEASUREMENT_SEPARATOR, this));
+        parameters.add(new BooleanP(SHOW_MEASUREMENTS, this, true));
+        parameters.add(new ObjMeasurementSelectorP(MEASUREMENTS, this));
 
     }
 
