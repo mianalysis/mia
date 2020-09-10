24a4d16c5b76f2831f9d35a8d39a749be6815a91
Stephen Cross <stephencross86@gmail.com>
2020-09-10 12:52:08 +0100

    A couple of minor updates before the final version

--- src/main/java/wbif/sjx/MIA/Module/ObjectProcessing/Refinement/CombineObjectSets.java
+++ src/main/java/wbif/sjx/MIA/Module/ObjectProcessing/Refinement/CombineObjectSets.java
@@ -4,6 +4,7 @@ import wbif.sjx.MIA.Module.Module;
 import wbif.sjx.MIA.Module.ModuleCollection;
 import wbif.sjx.MIA.Module.PackageNames;
 import wbif.sjx.MIA.Object.*;
+import wbif.sjx.MIA.Object.Parameters.ChoiceP;
 import wbif.sjx.MIA.Object.Parameters.InputObjectsP;
 import wbif.sjx.MIA.Object.Parameters.ParamSeparatorP;
 import wbif.sjx.MIA.Object.Parameters.ParameterCollection;
@@ -14,23 +15,44 @@ import wbif.sjx.MIA.Object.References.*;
  * Created by sc13967 on 31/01/2018.
  */
 public class CombineObjectSets extends Module {
-    public static final String INPUT_SEPARATOR = "Objects input/output";
+    public static final String INPUT_SEPARATOR = "Objects input";
     public static final String INPUT_OBJECTS_1 = "Input objects 1";
     public static final String INPUT_OBJECTS_2 = "Input objects 2";
+
+    public static final String OUTPUT_SEPARATOR = "Objects output";
+    public static final String OUTPUT_MODE = "Output mode";
     public static final String OUTPUT_OBJECTS = "Output objects";
 
+    public interface OutputModes {
+        String ADD_TO_OBJECTS_1 = "Add set objects 2 to set 1";
+        String ADD_TO_OBJECTS_2 = "Add set objects 1 to set 2";
+        String CREATE_NEW = "Create new object set";
 
-    public CombineObjectSets(ModuleCollection modules) {
-        super("Combine object sets",modules);
+        String[] ALL = new String[] { ADD_TO_OBJECTS_1, ADD_TO_OBJECTS_2, CREATE_NEW };
+
     }
 
-    static void combine(ObjCollection inputObjects, ObjCollection outputObjects) {
-        for (Obj obj:inputObjects.values()) {
+    public CombineObjectSets(ModuleCollection modules) {
+        super("Combine object sets", modules);
+    }
+
+    public static void combineAndCreate(ObjCollection inputObjects, ObjCollection outputObjects) {
+        for (Obj obj : inputObjects.values()) {
             int ID = outputObjects.getAndIncrementID();
-            Obj newObj = new Obj(outputObjects.getName(),ID,obj);
+            Obj newObj = new Obj(outputObjects.getName(), ID, obj);
             newObj.setCoordinateSet(obj.getCoordinateSet());
             newObj.setT(obj.getT());
             outputObjects.add(obj);
+        }
+    }
+
+    static void combineAndAdd(ObjCollection targetObjects, ObjCollection sourceObjects) {
+        for (Obj obj : sourceObjects.values()) {
+            int ID = targetObjects.getAndIncrementID();
+            Obj newObj = new Obj(targetObjects.getName(), ID, obj);
+            newObj.setCoordinateSet(obj.getCoordinateSet());
+            newObj.setT(obj.getT());
+            targetObjects.add(obj);
         }
     }
 
@@ -51,17 +73,40 @@ public class CombineObjectSets extends Module {
         ObjCollection inputObjects1 = workspace.getObjectSet(inputObjects1Name);
         String inputObjects2Name = parameters.getValue(INPUT_OBJECTS_2);
         ObjCollection inputObjects2 = workspace.getObjectSet(inputObjects2Name);
+        String outputMode = parameters.getValue(OUTPUT_MODE);
         String outputObjectsName = parameters.getValue(OUTPUT_OBJECTS);
-        ObjCollection outputObjects = new ObjCollection(outputObjectsName,inputObjects1);
 
         // Doing object merging
-        combine(inputObjects1,outputObjects);
-        combine(inputObjects2,outputObjects);
+        switch (outputMode) {
+            case OutputModes.ADD_TO_OBJECTS_1:
+                combineAndAdd(inputObjects1, inputObjects2);
 
-        // Adding the combined objects to the workspace
-        workspace.addObjects(outputObjects);
+                if (showOutput)
+                    inputObjects1.convertToImageRandomColours().showImage();
 
-        if (showOutput) outputObjects.convertToImageRandomColours().showImage();
+                break;
+
+            case OutputModes.ADD_TO_OBJECTS_2:
+                combineAndAdd(inputObjects2, inputObjects1);
+
+                if (showOutput)
+                    inputObjects2.convertToImageRandomColours().showImage();
+
+                break;
+
+            case OutputModes.CREATE_NEW:
+                ObjCollection outputObjects = new ObjCollection(outputObjectsName, inputObjects1);
+                combineAndCreate(inputObjects1, outputObjects);
+                combineAndCreate(inputObjects2, outputObjects);
+
+                // Adding the combined objects to the workspace
+                workspace.addObjects(outputObjects);
+
+                if (showOutput)
+                    outputObjects.convertToImageRandomColours().showImage();
+
+                break;
+        }
 
         return Status.PASS;
 
@@ -69,16 +114,34 @@ public class CombineObjectSets extends Module {
 
     @Override
     protected void initialiseParameters() {
-        parameters.add(new ParamSeparatorP(INPUT_SEPARATOR,this));
-        parameters.add(new InputObjectsP(INPUT_OBJECTS_1,this));
-        parameters.add(new InputObjectsP(INPUT_OBJECTS_2,this));
-        parameters.add(new OutputObjectsP(OUTPUT_OBJECTS,this));
+        parameters.add(new ParamSeparatorP(INPUT_SEPARATOR, this));
+        parameters.add(new InputObjectsP(INPUT_OBJECTS_1, this));
+        parameters.add(new InputObjectsP(INPUT_OBJECTS_2, this));
+
+        parameters.add(new ParamSeparatorP(OUTPUT_SEPARATOR, this));
+        parameters.add(new ChoiceP(OUTPUT_MODE, this, OutputModes.CREATE_NEW, OutputModes.ALL));
+        parameters.add(new OutputObjectsP(OUTPUT_OBJECTS, this));
 
     }
 
     @Override
     public ParameterCollection updateAndGetParameters() {
-        return parameters;
+        ParameterCollection returnedParameters = new ParameterCollection();
+
+        returnedParameters.add(parameters.get(INPUT_SEPARATOR));
+        returnedParameters.add(parameters.get(INPUT_OBJECTS_1));
+        returnedParameters.add(parameters.get(INPUT_OBJECTS_2));
+
+        returnedParameters.add(parameters.get(OUTPUT_SEPARATOR));
+        returnedParameters.add(parameters.get(OUTPUT_MODE));
+        switch ((String) parameters.getValue(OUTPUT_MODE)) {
+            case OutputModes.CREATE_NEW:
+                returnedParameters.add(parameters.get(OUTPUT_OBJECTS));
+                break;
+        }
+
+        return returnedParameters;
+
     }
 
     @Override
