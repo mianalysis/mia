3408eaa83c6a1557c32a98b52c61aa7214d46f46
Stephen Cross <stephencross86@gmail.com>
2020-09-22 06:44:33 +0100

    Added new legacy readers

---
+++ src/main/java/wbif/sjx/MIA/Process/AnalysisHandling/Legacy/AnalysisReader_0p10p0_0p15p0.java
@@ -0,0 +1,311 @@
+package wbif.sjx.MIA.Process.AnalysisHandling.Legacy;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import wbif.sjx.MIA.MIA;
+import wbif.sjx.MIA.GUI.GUI;
+import wbif.sjx.MIA.Module.Module;
+import wbif.sjx.MIA.Module.ModuleCollection;
+import wbif.sjx.MIA.Module.Hidden.InputControl;
+import wbif.sjx.MIA.Module.Hidden.OutputControl;
+import wbif.sjx.MIA.Object.Parameters.Abstract.Parameter;
+import wbif.sjx.MIA.Object.References.ImageMeasurementRef;
+import wbif.sjx.MIA.Object.References.MetadataRef;
+import wbif.sjx.MIA.Object.References.ObjMeasurementRef;
+import wbif.sjx.MIA.Process.ClassHunter;
+import wbif.sjx.MIA.Process.AnalysisHandling.Analysis;
+
+/**
+ * Created by sc13967 on 23/06/2017.
+ */
+public class AnalysisReader_0p10p0_0p15p0 {
+    public static Analysis loadAnalysis()
+            throws SAXException, IllegalAccessException, IOException, InstantiationException,
+            ParserConfigurationException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
+        FileDialog fileDialog = new FileDialog(new Frame(), "Select file to load", FileDialog.LOAD);
+        fileDialog.setMultipleMode(false);
+        fileDialog.setFile("*.mia");
+        fileDialog.setVisible(true);
+
+        if (fileDialog.getFiles().length == 0)
+            return null;
+
+        Analysis analysis = loadAnalysis(fileDialog.getFiles()[0]);
+        analysis.setAnalysisFilename(fileDialog.getFiles()[0].getAbsolutePath());
+
+        MIA.log.writeStatus("File loaded (" + FilenameUtils.getName(fileDialog.getFiles()[0].getName()) + ")");
+
+        return analysis;
+
+    }
+
+    public static Analysis loadAnalysis(File file)
+            throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException,
+            IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        String xml = FileUtils.readFileToString(file, "UTF-8");
+
+        return loadAnalysis(xml);
+
+    }
+
+    public static Analysis loadAnalysis(String xml)
+            throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException,
+            IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        MIA.log.writeStatus("Loading analysis");
+        GUI.updateProgressBar(0);
+
+        if (xml.startsWith("\uFEFF"))
+            xml = xml.substring(1);
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document doc = documentBuilder.parse(new InputSource(new ByteArrayInputStream(xml.getBytes("UTF-8"))));
+        doc.getDocumentElement().normalize();
+
+        Analysis analysis = new Analysis();
+        ModuleCollection modules = loadModules(doc);
+        analysis.setModules(modules);
+
+        return analysis;
+
+    }
+
+    public static ModuleCollection loadModules(Document doc)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ModuleCollection modules = new ModuleCollection();
+
+        // Creating a list of all available modules (rather than reading their full
+        // path, in case they move) using
+        // Reflections tool
+        List<String> availableModuleNames = ClassHunter.getModules(false);
+
+        NodeList moduleNodes = doc.getElementsByTagName("MODULE");
+        for (int i = 0; i < moduleNodes.getLength(); i++) {
+            Node moduleNode = moduleNodes.item(i);
+
+            // Creating an empty Module matching the input type. If none was found the loop
+            // skips to the next Module
+            Module module = initialiseModule(moduleNode, modules, availableModuleNames);
+            if (module == null)
+                continue;
+
+            module.setAttributesFromXML(moduleNode);
+
+            // If the module is an input, treat it differently
+            if (module.getClass().isInstance(new InputControl(modules))) {
+                modules.setInputControl((InputControl) module);
+            } else if (module.getClass().isInstance(new OutputControl(modules))) {
+                modules.setOutputControl((OutputControl) module);
+            } else {
+                modules.add(module);
+            }
+        }
+
+        return modules;
+
+    }
+
+    public static Module initialiseModule(Node moduleNode, ModuleCollection modules, List<String> availableModuleNames)
+            throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        NamedNodeMap moduleAttributes = moduleNode.getAttributes();
+        String className = moduleAttributes.getNamedItem("CLASSNAME").getNodeValue();
+        String moduleName = FilenameUtils.getExtension(className);
+
+        // Checking if this module has been reassigned
+        moduleName = MIA.lostAndFound.findModule(moduleName);
+
+        // Trying to load from available modules
+        for (String availableModuleName : availableModuleNames) {
+            if (moduleName.equals(FilenameUtils.getExtension(availableModuleName))) {
+                return initialiseModule(moduleNode, modules, availableModuleName);
+            }
+        }
+
+        // If no module was found matching that name an error message is displayed
+        MIA.log.writeWarning("Module \"" + moduleName + "\" not found (skipping)");
+
+        return null;
+
+    }
+
+    public static Module initialiseModule(Node moduleNode, ModuleCollection modules, String availableModuleName)
+            throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class<Module> clazz = null;
+        try {
+            clazz = (Class<Module>) Class.forName(availableModuleName);
+        } catch (ClassNotFoundException e) {
+            MIA.log.writeError(e);
+        }
+        Module module = (Module) clazz.getDeclaredConstructor(ModuleCollection.class).newInstance(modules);
+
+        // Populating parameters
+        NodeList moduleChildNodes = moduleNode.getChildNodes();
+        for (int i = 0; i < moduleChildNodes.getLength(); i++) {
+            switch (moduleChildNodes.item(i).getNodeName()) {
+                case "PARAMETERS":
+                    populateParameters(moduleChildNodes.item(i), module);
+                    break;
+
+                case "MEASUREMENTS":
+                    populateLegacyMeasurementRefs(moduleChildNodes.item(i), module);
+                    break;
+
+                case "IMAGE_MEASUREMENTS":
+                    populateImageMeasurementRefs(moduleChildNodes.item(i), module);
+                    break;
+
+                case "OBJECT_MEASUREMENTS":
+                    populateObjMeasurementRefs(moduleChildNodes.item(i), module);
+                    break;
+
+                case "METADATA":
+                    populateModuleMetadataRefs(moduleChildNodes.item(i), module);
+                    break;
+
+                case "RELATIONSHIPS":
+                case "PARENT_CHILD":
+                    populateModuleParentChildRefs(moduleChildNodes.item(i), module);
+                    break;
+            }
+        }
+
+        return module;
+
+    }
+
+    public static void populateParameters(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            Node referenceNode = referenceNodes.item(i);
+
+            // Getting measurement properties
+            NamedNodeMap attributes = referenceNode.getAttributes();
+            String parameterName = attributes.getNamedItem("NAME").getNodeValue();
+            Parameter parameter = module.getParameter(parameterName);
+
+            // If parameter isn't found, try the lost and found
+            if (parameter == null) {
+                String moduleName = module.getClass().getSimpleName();
+                parameterName = MIA.lostAndFound.findParameter(moduleName, parameterName);
+                parameter = module.getParameter(parameterName);
+            }
+
+            // If the parameter still isn't found, display a warning
+            if (parameter == null) {
+                String parameterValue = attributes.getNamedItem("VALUE").getNodeValue();
+                MIA.log.writeWarning("Parameter \"" + parameterName + "\" (value = \"" + parameterValue
+                        + "\") not found for module \"" + module.getName() + "\", skipping.");
+                continue;
+            }
+
+            parameter.setAttributesFromXML(referenceNode);
+
+        }
+    }
+
+    public static void populateLegacyMeasurementRefs(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            Node referenceNode = referenceNodes.item(i);
+
+            // Getting measurement properties
+            String type = referenceNode.getAttributes().getNamedItem("TYPE").getNodeValue();
+
+            // Acquiring the relevant reference
+            switch (type) {
+                case "IMAGE":
+                    ImageMeasurementRef imageMeasurementRef = new ImageMeasurementRef(referenceNode);
+                    module.addImageMeasurementRef(imageMeasurementRef);
+                    break;
+
+                case "OBJECTS":
+                    ObjMeasurementRef objMeasurementRef = new ObjMeasurementRef(referenceNode);
+                    module.addObjectMeasurementRef(objMeasurementRef);
+                    break;
+            }
+        }
+    }
+
+    public static void populateModuleMetadataRefs(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            // Getting measurement properties
+            MetadataRef ref = new MetadataRef(referenceNodes.item(i));
+            module.addMetadataRef(ref);
+        }
+    }
+
+    public static void populateImageMeasurementRefs(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            // Getting measurement properties
+            ImageMeasurementRef ref = new ImageMeasurementRef(referenceNodes.item(i));
+            module.addImageMeasurementRef(ref);
+        }
+    }
+
+    public static void populateObjMeasurementRefs(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            // Getting measurement properties
+            ObjMeasurementRef ref = new ObjMeasurementRef(referenceNodes.item(i));
+            module.addObjectMeasurementRef(ref);
+        }
+    }
+
+    public static void populateModuleParentChildRefs(Node moduleNode, Module module) {
+        NodeList referenceNodes = moduleNode.getChildNodes();
+
+        // Iterating over all references of this type
+        for (int i = 0; i < referenceNodes.getLength(); i++) {
+            Node node = referenceNodes.item(i);
+
+            switch (node.getNodeName()) {
+                case "RELATIONSHIP":
+                case "PARENT_CHILD":
+                    MIA.log.writeDebug("Handle parent/child relationship");
+                    // // Getting relationship properties
+                    // ParentChildRef parentChildRef = new ParentChildRef(node);
+                    // module.addParentChildRef(parentChildRef);
+                    break;
+
+                case "PARTNER":
+                MIA.log.writeDebug("Handle partner relationship");
+                    // // Getting relationship properties
+                    // PartnerRef partnerRef = new PartnerRef(node);
+                    // module.addPartnerRef(partnerRef);
+                    break;
+            }
+        }
+    }
+}
