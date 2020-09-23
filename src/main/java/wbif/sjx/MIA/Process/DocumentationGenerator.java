2900deced2ecb92b5900182264db4d92a2dfe20a
Stephen Cross <stephencross86@gmail.com>
2020-09-23 15:38:46 +0100

    Updated GitHub Pages content

--- src/main/java/wbif/sjx/MIA/Process/DocumentationGenerator.java
+++ src/main/java/wbif/sjx/MIA/Process/DocumentationGenerator.java
@@ -31,17 +31,17 @@ import wbif.sjx.MIA.Macro.MacroHandler;
 import wbif.sjx.MIA.Macro.MacroOperation;
 import wbif.sjx.MIA.Module.Module;
 import wbif.sjx.MIA.Module.ModuleCollection;
-import wbif.sjx.MIA.Object.Parameters.ChoiceP;
 import wbif.sjx.MIA.Object.Parameters.Abstract.Parameter;
 
 public class DocumentationGenerator {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         try {
             // Creating README.md
             generateReadmeMarkdown();
 
             // Creating website content
             generateIndexPage();
+            generateGettingStartedPage();
             generateModuleList();
             generateModulePages();
             generateMacroList();
@@ -56,7 +56,7 @@ public class DocumentationGenerator {
         // Generate module list HTML document
         String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/index.html")));
         String indexContent = getIndexContent();
-        template = template.replace("${INSERT}",indexContent);
+        template = template.replace("${INSERT}", indexContent);
 
         FileWriter writer = new FileWriter("docs/index.html");
         writer.write(template);
@@ -64,11 +64,25 @@ public class DocumentationGenerator {
         writer.close();
     }
 
+    private static void generateGettingStartedPage() throws IOException {
+        // Generate module list HTML document
+        String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/gettingstarted.html")));
+        String gettingStartedContent = getGettingStartedContent();
+        template = template.replace("${INSERT}", gettingStartedContent);
+
+        new File("docs/html/").mkdirs();
+        FileWriter writer = new FileWriter("docs/html/gettingstarted.html");
+        writer.write(template);
+        writer.flush();
+        writer.close();
+
+    }
+
     private static void generateModuleList() throws IOException {
         // Generate module list HTML document
         String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/modulelist.html")));
         String moduleList = getModuleList();
-        template = template.replace("${INSERT}",moduleList);
+        template = template.replace("${INSERT}", moduleList);
 
         new File("docs/html/").mkdirs();
         FileWriter writer = new FileWriter("docs/html/modulelist.html");
@@ -80,14 +94,14 @@ public class DocumentationGenerator {
 
     private static void generateModulePages() throws IOException {
         HashSet<Module> modules = getModules();
-        for (Module module:modules) {
+        for (Module module : modules) {
             String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/modules.html")));
             String moduleList = getModuleSummary(module);
-            template = template.replace("${INSERT}",moduleList);
+            template = template.replace("${INSERT}", moduleList);
 
             new File("docs/html/modules/").mkdirs();
 
-            FileWriter writer = new FileWriter("docs/html/"+getSimpleModulePath(module));
+            FileWriter writer = new FileWriter("docs/html/" + getSimpleModulePath(module));
             writer.write(template);
             writer.flush();
             writer.close();
@@ -99,7 +113,7 @@ public class DocumentationGenerator {
         // Generate module list HTML document
         String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/macrolist.html")));
         String macroList = getMacroList();
-        template = template.replace("${INSERT}",macroList);
+        template = template.replace("${INSERT}", macroList);
 
         new File("docs/html/").mkdirs();
         FileWriter writer = new FileWriter("docs/html/macrolist.html");
@@ -110,20 +124,21 @@ public class DocumentationGenerator {
     }
 
     private static void generateMacroPages() throws IOException {
-//        ArrayList<MacroOperation> macros = MacroHandler.getMacroOperations();
-//        for (MacroOperation macro:macros) {
-//            String template = new String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/macros.html")));
-//            String macroList = getMacroSummary(macro);
-//            template = template.replace("${INSERT}",macroList);
-//
-//            new File("docs/html/macros/").mkdirs();
-//
-//            FileWriter writer = new FileWriter("docs/html/"+getSimpleMacroPath(macro));
-//            writer.write(template);
-//            writer.flush();
-//            writer.close();
-//
-//        }
+        ArrayList<MacroOperation> macros = MacroHandler.getMacroOperations();
+        for (MacroOperation macro:macros) {
+        String template = new
+        String(Files.readAllBytes(Paths.get("src/main/resources/templatehtml/macros.html")));
+        String macroList = getMacroSummary(macro);
+        template = template.replace("${INSERT}",macroList);
+        
+        new File("docs/html/macros/").mkdirs();
+        
+        FileWriter writer = new FileWriter("docs/html/"+getSimpleMacroPath(macro));
+        writer.write(template);
+        writer.flush();
+        writer.close();
+        
+        }
     }
 
     private static String getIndexContent() {
@@ -133,14 +148,6 @@ public class DocumentationGenerator {
 
         try {
             String string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/introduction.md")));
-            sb.append(renderer.render(parser.parse(string)));
-            sb.append("<br><br>");
-
-            string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/installation.md")));
-            sb.append(renderer.render(parser.parse(string)));
-            sb.append("<br><br>");
-
-            string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/gettingStarted.md")));
             sb.append(renderer.render(parser.parse(string)));
             sb.append("<br><br>");
 
@@ -163,6 +170,32 @@ public class DocumentationGenerator {
         return sb.toString();
 
     }
+    
+    private static String getGettingStartedContent() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            String string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/installation.md")));
+            sb.append(renderer.render(parser.parse(string)));
+            sb.append("<br><br>");
+
+            string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/creatingWorkflow.md")));
+            sb.append(renderer.render(parser.parse(string)));
+            sb.append("<br><br>");
+            
+            string = new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/usingExistingWorkflow.md")));
+            sb.append(renderer.render(parser.parse(string)));
+            sb.append("<br><br>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+
+    }
 
     private static String getModuleSummary(Module module) {
         StringBuilder sb = new StringBuilder();
@@ -171,55 +204,20 @@ public class DocumentationGenerator {
         sb.append("<a href=\"../modulelist.html\">Back to module list</a>\r\n");
 
         // Adding the Module title
-        sb.append("<h1>")
-                .append(module.getName())
-                .append("</h1>\r\n");
+        sb.append("<h1>").append(module.getName()).append("</h1>\r\n");
 
         // Adding the Module summary
         String helpText = module.getDescription();
         helpText = helpText == null ? "" : helpText;
-        sb.append("<h2>Description</h2>\r\n")
-                .append(helpText)
-                .append("\r\n");
+        sb.append("<h2>Description</h2>\r\n").append(helpText).append("\r\n");
 
         sb.append("<h2>Parameters</h2>\r\n<ul>");
-        for (Parameter parameter:module.getAllParameters().values()) {
-            if (!parameter.isExported()) continue;
-            sb.append("<li>")
-                    .append(parameter.getName())
-                    .append("<ul>");
-
-            if (!parameter.getDescription().equals("")) {
-                sb.append("<li>Description: ")
-                        .append(parameter.getDescription())
-                        .append("</li>");
-            }
-
-            sb.append("<li>Type: ")
-                    .append(parameter.getClass().getSimpleName())
-                    .append("</li>");
-
-            if (!parameter.getRawStringValue().equals("")) {
-                sb.append("<li>Default value: ")
-                        .append(parameter.getRawStringValue())
-                        .append("</li>");
-            }
-
-            if (parameter instanceof ChoiceP) {
-                String[] choices = ((ChoiceP) parameter).getChoices();
-                sb.append("<li>Choices: ")
-                        .append("<ul>");
-
-                for (String choice:choices) {
-                    sb.append("<li>")
-                            .append(choice)
-                            .append("</li>");
-                }
-                sb.append("</ul></li>");
-            }
-
-            sb.append("</ul><br>");
-
+        for (Parameter parameter : module.getAllParameters().values()) {
+            if (!parameter.isExported())
+                continue;
+            sb.append("<li><b>").append(parameter.getName()).append("</b> (default = \"")
+                    .append(parameter.getRawStringValue()).append("\") ").append(parameter.getDescription())
+                    .append("</li><br>");
         }
         sb.append("</ul>");
 
@@ -235,29 +233,18 @@ public class DocumentationGenerator {
         sb.append("<a href=\"../macrolist.html\">Back to macro list</a>\r\n");
 
         // Adding the MacroOperation title
-        sb.append("<h1>")
-                .append(macro.getName())
-                .append("</h1>\r\n");
+        sb.append("<h1>").append(macro.getName()).append("</h1>\r\n");
 
         // Adding the Module summary
         String helpText = macro.getDescription();
         helpText = helpText == null ? "" : helpText;
-        sb.append("<h2>Description</h2>\r\n")
-                .append(helpText)
-                .append("\r\n");
+        sb.append("<h2>Description</h2>\r\n").append(helpText).append("\r\n");
 
-        sb.append("<h2>Parameters</h2>\r\n");
-
+        sb.append("<h2>Parameters</h2>\r\n<ul>");
         String parameterList = macro.getArgumentsDescription();
-        String[] parameters = parameterList.split(",");
-        if (parameters.length > 0) sb.append("<ul");
-
-        for (String parameter:parameters) {
-            sb.append("<li>")
-                    .append(parameter)
-                    .append("</li>");
-
-        }
+        for (String parameter : parameterList.split(",")) {
+            sb.append("<li>").append(parameter).append("</li><br>");
+        }        
         sb.append("</ul>");
 
         // Generate module list HTML document
@@ -273,26 +260,21 @@ public class DocumentationGenerator {
         LinkedHashSet<Module> modules = getModules();
         TreeSet<String> packageNames = new TreeSet<>();
 
-        for (Module module:modules) {
+        for (Module module : modules) {
             packageNames.add(module.getPackageName());
         }
 
         // For each package name, adding a list of the matching Modules
-        for (String packageName:packageNames) {
-            String prettyPackageName = packageName.replace("\\"," / ");
-            sb.append("<h2>")
-                    .append(prettyPackageName)
-                    .append("</h2>\r\n")
-                    .append("<ul>\r\n");
+        for (String packageName : packageNames) {
+            String prettyPackageName = packageName.replace("\\", " / ");
+            sb.append("<h2>").append(prettyPackageName).append("</h2>\r\n").append("<ul>\r\n");
 
             // For each Module in this package, create a link to the description document
-            for (Module module:modules) {
-                if (!module.getPackageName().equals(packageName)) continue;
+            for (Module module : modules) {
+                if (!module.getPackageName().equals(packageName))
+                    continue;
 
-                sb.append("<li><a href=\".")
-                        .append(getSimpleModulePath(module))
-                        .append("\">")
-                        .append(module.getName())
+                sb.append("<li><a href=\".").append(getSimpleModulePath(module)).append("\">").append(module.getName())
                         .append("</a></li>\r\n");
 
             }
@@ -313,11 +295,8 @@ public class DocumentationGenerator {
         ArrayList<MacroOperation> macros = MacroHandler.getMacroOperations();
 
         // For each package name, adding a list of the matching Modules
-        for (MacroOperation macro:macros) {
-            sb.append("<li><a href=\".")
-                    .append(getSimpleMacroPath(macro))
-                    .append("\">")
-                    .append(macro.getName())
+        for (MacroOperation macro : macros) {
+            sb.append("<li><a href=\".").append(getSimpleMacroPath(macro)).append("\">").append(macro.getName())
                     .append("</a></li>\r\n");
         }
 
@@ -333,16 +312,18 @@ public class DocumentationGenerator {
 
         // Converting the list of classes to a list of Modules
         LinkedHashSet<Module> modules = new LinkedHashSet<>();
-        for (String className:classNames) {
+        for (String className : classNames) {
             try {
                 Class<Module> clazz = (Class<Module>) Class.forName(className);
 
                 // Skip any abstract Modules
-                if (Modifier.isAbstract(clazz.getModifiers())) continue;
+                if (Modifier.isAbstract(clazz.getModifiers()))
+                    continue;
 
                 Constructor constructor = clazz.getDeclaredConstructor(ModuleCollection.class);
                 modules.add((Module) constructor.newInstance(new ModuleCollection()));
-            } catch (ClassNotFoundException |InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException
+                    | InvocationTargetException e) {
                 MIA.log.writeError(e);
             }
         }
@@ -355,13 +336,10 @@ public class DocumentationGenerator {
         StringBuilder sb = new StringBuilder();
 
         String simplePackageName = module.getPackageName();
-        simplePackageName = simplePackageName.replaceAll("[^A-Za-z0-9]","");
-        simplePackageName = simplePackageName.replace(".","");
+        simplePackageName = simplePackageName.replaceAll("[^A-Za-z0-9]", "");
+        simplePackageName = simplePackageName.replace(".", "");
 
-        sb.append("/modules/")
-                .append(simplePackageName)
-                .append(module.getClass().getSimpleName())
-                .append(".html");
+        sb.append("/modules/").append(simplePackageName).append(module.getClass().getSimpleName()).append(".html");
 
         return sb.toString();
 
@@ -371,12 +349,10 @@ public class DocumentationGenerator {
         StringBuilder sb = new StringBuilder();
 
         String simpleName = macro.getName();
-        simpleName = simpleName.replaceAll("[^A-Za-z0-9]","");
-        simpleName = simpleName.replace(".","");
+        simpleName = simpleName.replaceAll("[^A-Za-z0-9]", "");
+        simpleName = simpleName.replace(".", "");
 
-        sb.append("/macros/")
-                .append(simpleName)
-                .append(".html");
+        sb.append("/macros/").append(simpleName).append(".html");
 
         return sb.toString();
 
@@ -389,13 +365,16 @@ public class DocumentationGenerator {
             sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/githubBadges.md"))));
             sb.append("\n\n");
 
-            sb.append("[![Wolfson Bioimaging](./src/main/resources/Images/Logo_text_UoB_128.png)](http://www.bristol.ac.uk/wolfson-bioimaging/)");
+            sb.append(
+                    "[![Wolfson Bioimaging](./src/main/resources/Images/Logo_text_UoB_128.png)](http://www.bristol.ac.uk/wolfson-bioimaging/)");
             sb.append("\n\n");
             sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/introduction.md"))));
             sb.append("\n\n");
             sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/installation.md"))));
             sb.append("\n\n");
-            sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/gettingStarted.md"))));
+            sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/creatingWorkflow.md"))));
+            sb.append("\n\n");
+            sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/usingExistingWorkflow.md"))));
             sb.append("\n\n");
             sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/acknowledgements.md"))));
             sb.append("\n\n");
@@ -403,7 +382,7 @@ public class DocumentationGenerator {
             sb.append("\n\n");
             sb.append(new String(Files.readAllBytes(Paths.get("src/main/resources/templatemd/note.md"))));
             sb.append("\n\n");
-            
+
             FileWriter writer = new FileWriter("README.md");
             writer.write(sb.toString());
             writer.flush();
@@ -419,7 +398,8 @@ public class DocumentationGenerator {
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         StringBuilder sb = new StringBuilder();
 
-        // The following is required to get the version number and release date from the pom.xml
+        // The following is required to get the version number and release date from the
+        // pom.xml
         String version = "";
         try {
             FileReader reader = new FileReader("pom.xml");
@@ -441,7 +421,7 @@ public class DocumentationGenerator {
 
             URL url = Resources.getResource("templatemd/introduction.md");
             String string = Resources.toString(url, Charsets.UTF_8);
-            string = string.replace("${version}",version);
+            string = string.replace("${version}", version);
             sb.append(renderer.render(parser.parse(string)));
             sb.append("<br><br>");
 
@@ -488,7 +468,12 @@ public class DocumentationGenerator {
             sb.append(renderer.render(parser.parse(string)));
             sb.append("<br><br>");
 
-            url = Resources.getResource("templatemd/gettingStarted.md");
+            url = Resources.getResource("templatemd/creatingWorkflow.md");
+            string = Resources.toString(url, Charsets.UTF_8);
+            sb.append(renderer.render(parser.parse(string)));
+            sb.append("<br><br>");
+
+            url = Resources.getResource("templatemd/usingExistingWorkflow.md");
             string = Resources.toString(url, Charsets.UTF_8);
             sb.append(renderer.render(parser.parse(string)));
             sb.append("<br><br>");
