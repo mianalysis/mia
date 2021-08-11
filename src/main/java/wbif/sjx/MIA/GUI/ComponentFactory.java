4e1e68a692c70b5f34b7343908a87f5da7117092
Stephen Cross <stephencross86@gmail.com>
2021-08-11 11:21:22 +0100

    Re-enabled option to disable sections from the basic view
    The power icons to the left of a GUI Separator allow these sections to
    be turned on/off.

--- src/main/java/wbif/sjx/MIA/GUI/ComponentFactory.java
+++ src/main/java/wbif/sjx/MIA/GUI/ComponentFactory.java
@@ -38,9 +38,9 @@ import wbif.sjx.MIA.Module.Miscellaneous.GUISeparator;
 import wbif.sjx.MIA.Object.Colours;
 import wbif.sjx.MIA.Object.Parameters.BooleanP;
 import wbif.sjx.MIA.Object.Parameters.ObjMeasurementSelectorP;
-import wbif.sjx.MIA.Object.Parameters.SeparatorP;
 import wbif.sjx.MIA.Object.Parameters.ParameterCollection;
 import wbif.sjx.MIA.Object.Parameters.ParameterGroup;
+import wbif.sjx.MIA.Object.Parameters.SeparatorP;
 import wbif.sjx.MIA.Object.Parameters.Abstract.Parameter;
 import wbif.sjx.MIA.Object.Parameters.Text.MessageP;
 import wbif.sjx.MIA.Object.Parameters.Text.TextAreaP;
@@ -186,7 +186,7 @@ public class ComponentFactory {
 
         DisableableCheck disableableCheck = new DisableableCheck(activeModule);
         disableableCheck.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
-        if (activeModule.getClass() == InputControl.class || activeModule.getClass() == GUISeparator.class) {
+        if (activeModule.getClass() == InputControl.class) {
             disableableCheck.setEnabled(false);
             disableableCheck.setOpaque(false);
         }
@@ -243,14 +243,14 @@ public class ComponentFactory {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.WEST;
 
-        // ModuleEnabledButton moduleEnabledButton = new ModuleEnabledButton(module);
-        // moduleEnabledButton.setPreferredSize(new Dimension(elementHeight, elementHeight));
-        // moduleEnabledButton.setMinimumSize(new Dimension(elementHeight, elementHeight));
-        // moduleEnabledButton.setEnabled(module.canBeDisabled());
-        // moduleEnabledButton.setBorderPainted(false);
-        // moduleEnabledButton.setOpaque(false);
-        // moduleEnabledButton.setContentAreaFilled(false);
-        // panel.add(moduleEnabledButton, c);
+        ModuleEnabledButton moduleEnabledButton = new ModuleEnabledButton(module);
+        moduleEnabledButton.setPreferredSize(new Dimension(elementHeight, elementHeight));
+        moduleEnabledButton.setMinimumSize(new Dimension(elementHeight, elementHeight));
+        moduleEnabledButton.setEnabled(module.canBeDisabled());
+        moduleEnabledButton.setBorderPainted(false);
+        moduleEnabledButton.setOpaque(false);
+        moduleEnabledButton.setContentAreaFilled(false);
+        panel.add(moduleEnabledButton, c);
 
         BooleanP expandedBasic = (BooleanP) module.getParameter(GUISeparator.EXPANDED_BASIC);
         JLabel leftArrowLabel = new JLabel();
@@ -265,8 +265,8 @@ public class ComponentFactory {
             leftArrowLabel.setIcon(rightArrow);
         }
         // }
-        c.insets = new Insets(0, 5, 0, 0);
-        // c.gridx++;
+        c.insets = new Insets(0, 0, 0, 5);
+        c.gridx++;
         panel.add(leftArrowLabel, c);
 
         JSeparator separatorLeft = new JSeparator();
