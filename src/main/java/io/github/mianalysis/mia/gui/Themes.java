4dc4b8c6e58f9a952769a173a008b7b113c91e67
Stephen Cross <stephencross86@gmail.com>
2023-04-19 18:33:29 +0100

    Added placeholder code for FlatLaf 3.1

--- src/main/java/io/github/mianalysis/mia/gui/Themes.java
+++ src/main/java/io/github/mianalysis/mia/gui/Themes.java
@@ -10,17 +10,25 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+// import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+// import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 public interface Themes {
     String FLAT_LAF_DARK = "Flat LAF (dark)";
     String FLAT_LAF_DARKULA = "Flat LAF (darkula)";
     String FLAT_LAF_INTELLIJ = "Flat LAF (IntelliJ)";
     String FLAT_LAF_LIGHT = "Flat LAF (light)";
+    // String FLAT_LAF_MACOS_DARK = "Flat LAF (macOS dark)";
+    // String FLAT_LAF_MACOS_LIGHT = "Flat LAF (macOS light)";
     String MATCH_IMAGEJ = "Match ImageJ";
     String SYSTEM_DEFAULT = "System default";
 
-    String[] ALL = new String[] { FLAT_LAF_DARK, FLAT_LAF_DARKULA, FLAT_LAF_INTELLIJ, FLAT_LAF_LIGHT, MATCH_IMAGEJ,
-            SYSTEM_DEFAULT };
+    // String[] ALL = new String[] { FLAT_LAF_DARK, FLAT_LAF_DARKULA, FLAT_LAF_INTELLIJ, FLAT_LAF_LIGHT,
+    //         FLAT_LAF_MACOS_DARK, FLAT_LAF_MACOS_LIGHT, MATCH_IMAGEJ,
+    //         SYSTEM_DEFAULT };
+            String[] ALL = new String[] { FLAT_LAF_DARK, FLAT_LAF_DARKULA, FLAT_LAF_INTELLIJ, FLAT_LAF_LIGHT,
+                MATCH_IMAGEJ,
+                SYSTEM_DEFAULT };
 
     static String defaultTheme = FLAT_LAF_LIGHT;
     static LookAndFeel ijLAF = UIManager.getLookAndFeel();
@@ -38,6 +46,10 @@ public interface Themes {
                 return new FlatIntelliJLaf();
             case Themes.FLAT_LAF_LIGHT:
                 return new FlatLightLaf();
+            // case Themes.FLAT_LAF_MACOS_DARK:
+            //     return new FlatMacDarkLaf();
+            // case Themes.FLAT_LAF_MACOS_LIGHT:
+            //     return new FlatMacLightLaf();
             case Themes.MATCH_IMAGEJ:
                 Class<?> clazz = Class.forName(ijLAF.getClass().getCanonicalName());
                 Constructor<?> ctor = clazz.getConstructor();
