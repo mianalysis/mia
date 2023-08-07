c5ce05c75d672735f80f02748abe6975dffc2968
Stephen Cross <stephencross86@gmail.com>
2023-08-07 15:33:46 +0100

    Updated RidgeDetection documentation

--- src/main/java/io/github/mianalysis/mia/module/objects/detect/RidgeDetection.java
+++ src/main/java/io/github/mianalysis/mia/module/objects/detect/RidgeDetection.java
@@ -787,7 +787,8 @@ public class RidgeDetection extends Module {
                 "When selected, the output ridge objects will have the width applied (rather than having single pixel width).");
 
         parameters.get(OVERLAP_MODE).setDescription(
-                "Controls how intersecting lines should be handled.  For more information see <a href=\"https://imagej.net/Ridge_Detection.html#Overlap_resolution\"/>.<br><ul>"
+                "Controls how intersecting lines should be handled.  For more information see <a href=\"https://imagej.net/Ridge_Detection.html#Overlap_resolution\">                \"Controls how intersecting lines should be handled.  For more information see <a href=\\\"https://imagej.net/Ridge_Detection.html#Overlap_resolution\\\">.<br><ul>\"\n" + //
+                        ".<br><ul>"
 
                         + "<li>\"" + OverlapModes.NONE
                         + "\" Ridges are terminated at line intersections, so two overlapping ridges will likely be identified as at least four separate ridge objects.</li>"
