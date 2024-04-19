ba34711e6222ed3b7b165beee009ac33efaa8c87
Stephen Cross <stephencross86@gmail.com>
2024-04-19 16:26:02 +0100

    Added text entry mode to choice parameter boxes
    These allow fixed text values (e.g. global parameters) to be used.

--- mia-core/src/main/java/io/github/mianalysis/mia/object/parameters/abstrakt/ChoiceType.java
+++ mia-core/src/main/java/io/github/mianalysis/mia/object/parameters/abstrakt/ChoiceType.java
@@ -1,15 +1,19 @@
 package io.github.mianalysis.mia.object.parameters.abstrakt;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 import io.github.mianalysis.mia.MIA;
 import io.github.mianalysis.mia.module.Module;
+import io.github.mianalysis.mia.module.system.GlobalVariables;
 import io.github.mianalysis.mia.object.Workspace;
 import io.github.mianalysis.mia.process.ParameterControlFactory;
 
 public abstract class ChoiceType extends Parameter {
     protected String choice = "";
+    protected boolean showChoice = true; // When true GUI shows a choice drop-down, when false GUI shows a text entry
+                                         // box
 
     public ChoiceType(String name, Module module) {
         super(name, module);
@@ -21,14 +25,24 @@ public abstract class ChoiceType extends Parameter {
 
     public String getChoice() {
         return choice;
+
     }
 
     public void setChoice(String choice) {
-        if (choice == null) choice = "";
+        if (choice == null)
+            choice = "";
         this.choice = choice;
     }
 
     public abstract String[] getChoices();
+
+    public void setShowChoice(boolean showChoice) {
+        this.showChoice = showChoice;
+    }
+
+    public boolean isShowChoice() {
+        return this.showChoice;
+    }
 
     @Override
     public String getRawStringValue() {
@@ -37,7 +51,8 @@ public abstract class ChoiceType extends Parameter {
 
     @Override
     public void setValueFromString(String string) {
-        if (string == null) string = "";
+        if (string == null)
+            string = "";
 
         this.choice = string;
 
@@ -50,7 +65,12 @@ public abstract class ChoiceType extends Parameter {
 
     @Override
     public <T> T getValue(Workspace workspace) {
-        return (T) choice;
+        String converted = GlobalVariables.convertString(choice, module.getModules());
+        converted = TextType.insertWorkspaceValues(converted, workspace);
+        converted = TextType.applyCalculation(converted);
+
+        return (T) converted;
+
     }
 
     @Override
@@ -59,31 +79,47 @@ public abstract class ChoiceType extends Parameter {
     }
 
     @Override
+    public void appendXMLAttributes(Element element) {
+        super.appendXMLAttributes(element);
+
+        element.setAttribute("SHOW_CHOICE", Boolean.toString(isShowChoice()));
+
+    }
+
+    @Override
     public void setAttributesFromXML(Node node) {
         super.setAttributesFromXML(node);
 
         NamedNodeMap map = node.getAttributes();
-        
-        // ChoiceType values can be from hard-coded sources, so we check if they're labelled for reassignment.
+
+        // ChoiceType values can be from hard-coded sources, so we check if they're
+        // labelled for reassignment.
         String xmlValue = map.getNamedItem("VALUE").getNodeValue();
         xmlValue = MIA.getLostAndFound().findParameterValue(module.getClass().getSimpleName(), getName(), xmlValue);
         setValueFromString(xmlValue);
 
         setVisible(Boolean.parseBoolean(map.getNamedItem("VISIBLE").getNodeValue()));
 
+        if (map.getNamedItem("SHOW_CHOICE") != null)
+            setShowChoice(Boolean.parseBoolean(map.getNamedItem("SHOW_CHOICE").getNodeValue()));
+
     }
 
     @Override
     public boolean verify() {
-        // Verifying the choice is present in the choices.  When we generateModuleList getChoices, we should be getting the valid
-        // options only.
-        String[] choices = getChoices();
+        if (isShowChoice()) {
+            // Verifying the choice is present in the choices. When we generateModuleList
+            // getChoices, we should be getting the valid
+            // options only.
+            String[] choices = getChoices();
 
-        for (String currChoice:choices) {
-            if (choice.equals(currChoice)) return true;
+            for (String currChoice : choices)
+                if (choice.equals(currChoice))
+                    return true;
+
+            return false;
+        } else {
+            return GlobalVariables.variablesPresent(getRawStringValue(), module.getModules());
         }
-
-        return false;
-
     }
 }
--- mia-plugin/src/main/java/io/github/mianalysis/mia/gui/parametercontrols/ChoiceArrayParameter.java
+++ mia-plugin/src/main/java/io/github/mianalysis/mia/gui/parametercontrols/ChoiceArrayParameter.java
@@ -3,9 +3,17 @@ package io.github.mianalysis.mia.gui.parametercontrols;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.MouseInfo;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
 
 import io.github.mianalysis.mia.gui.GUI;
 import io.github.mianalysis.mia.module.core.OutputControl;
@@ -15,21 +23,45 @@ import io.github.mianalysis.mia.object.parameters.abstrakt.ParameterControl;
 /**
  * Created by sc13967 on 22/05/2017.
  */
-public class ChoiceArrayParameter extends ParameterControl implements ActionListener {
-    private WiderDropDownCombo control;
+public class ChoiceArrayParameter extends ParameterControl implements ActionListener, FocusListener, MouseListener {
+    private WiderDropDownCombo choiceControl;
+    private JTextField textControl;
+    private ToggleModeMenu toggleModeMenu = new ToggleModeMenu();
 
     public ChoiceArrayParameter(ChoiceType parameter) {
         super(parameter);
 
-        // Choices may have not been initialised when this first runs, so a blank list is created
+        // Choices may have not been initialised when this first runs, so a blank list
+        // is created
         String[] choices = parameter.getChoices();
-        if (choices == null) choices = new String[]{""};
-        control = new WiderDropDownCombo(choices);
+        if (choices == null)
+            choices = new String[] { "" };
+        choiceControl = new WiderDropDownCombo(choices);
 
-        control.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
-        control.setSelectedItem(parameter.getChoice());
-        control.addActionListener(this);
-        control.setWide(true);
+        choiceControl.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+        choiceControl.setSelectedItem(parameter.getChoice());
+        choiceControl.addActionListener(this);
+        choiceControl.addMouseListener(this);
+        choiceControl.setWide(true);
+
+        textControl = new JTextField();
+        textControl.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+        textControl.setText(parameter.getRawStringValue());
+        textControl.addFocusListener(this);
+        textControl.addMouseListener(this);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // Only display menu if the right mouse button is clicked
+        if (e.getButton() != MouseEvent.BUTTON3)
+            return;
+
+        // Populating the list containing all available modules
+        toggleModeMenu.show(GUI.getFrame(), 0, 0);
+        toggleModeMenu.setLocation(MouseInfo.getPointerInfo().getLocation());
+        toggleModeMenu.setVisible(true);
 
     }
 
@@ -37,40 +69,129 @@ public class ChoiceArrayParameter extends ParameterControl implements ActionList
     public void actionPerformed(ActionEvent e) {
         GUI.addUndo();
 
-        ((ChoiceType) parameter).setChoice((String) control.getSelectedItem());
+        ((ChoiceType) parameter).setValueFromString((String) choiceControl.getSelectedItem());
 
         int idx = GUI.getModules().indexOf(parameter.getModule());
         if (idx <= GUI.getLastModuleEval() & !(parameter.getModule() instanceof OutputControl))
             GUI.setLastModuleEval(idx - 1);
 
-            GUI.updateModules();
-            GUI.updateParameters();
+        GUI.updateModules();
+        GUI.updateParameters();
 
         updateControl();
 
     }
 
     @Override
+    public void focusGained(FocusEvent e) {
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        GUI.addUndo();
+
+        parameter.setValueFromString(textControl.getText());
+
+        int idx = GUI.getModules().indexOf(parameter.getModule());
+        if (idx <= GUI.getLastModuleEval() & !(parameter.getModule() instanceof OutputControl))
+            GUI.setLastModuleEval(idx - 1);
+
+        // GUI.updateParameters();
+        GUI.updateModuleStates();
+        updateControl();
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
     public JComponent getComponent() {
-        return control;
+        if (((ChoiceType) parameter).isShowChoice())
+            return choiceControl;
+        else
+            return textControl;
     }
 
     @Override
     public void updateControl() {
-        String[] choices = ((ChoiceType) parameter).getChoices();
-        if (choices == null) choices = new String[]{""};
+        if (((ChoiceType) parameter).isShowChoice()) {
+            String[] choices = ((ChoiceType) parameter).getChoices();
+            if (choices == null)
+                choices = new String[] { "" };
 
-        // Getting previously-selected item
-        String selected = (String) control.getSelectedItem();
+            // Getting previously-selected item
+            String selected = (String) choiceControl.getSelectedItem();
 
-        // Creating a new model
-        DefaultComboBoxModel<String> model = new DefaultComboBoxModel(choices);
-        model.setSelectedItem(selected);
+            // Creating a new model
+            DefaultComboBoxModel<String> model = new DefaultComboBoxModel(choices);
+            model.setSelectedItem(selected);
 
-        // Updating the control
-        control.setModel(model);
-        control.repaint();
-        control.revalidate();
+            // Updating the control
+            choiceControl.setModel(model);
+            choiceControl.repaint();
+            choiceControl.revalidate();
 
+        }
     }
+
+    class ToggleModeMenu extends JPopupMenu implements ActionListener {
+        private static final String CHANGE_TO_TEXT = "Change to text entry";
+        private static final String CHANGE_TO_CHOICE = "Change to choice";
+
+        private JMenuItem menuItem = new JMenuItem();
+
+        public ToggleModeMenu() {
+            if (((ChoiceType) parameter).isShowChoice())
+                menuItem.setText(CHANGE_TO_TEXT);
+            else
+                menuItem.setText(CHANGE_TO_CHOICE);
+
+            menuItem.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+            menuItem.addActionListener(this);
+
+            add(menuItem);
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            GUI.addUndo();
+            setVisible(false);
+
+            switch (e.getActionCommand()) {
+                case CHANGE_TO_TEXT:
+                    ((ChoiceType) parameter).setShowChoice(false);
+                    menuItem.setText(CHANGE_TO_CHOICE);
+                    break;
+                case CHANGE_TO_CHOICE:
+                    ((ChoiceType) parameter).setShowChoice(true);
+                    menuItem.setText(CHANGE_TO_TEXT);
+                    break;
+            }
+
+            GUI.updateModules();
+            GUI.updateParameters();
+
+        }
+    }
+
 }
