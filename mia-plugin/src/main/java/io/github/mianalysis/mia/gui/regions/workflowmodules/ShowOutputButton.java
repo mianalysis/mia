3897cc98bd9bc3ce5bfe0f3511159da7582d6f62
Stephen Cross <stephencross86@gmail.com>
2024-08-21 20:05:34 +0100

    Updated ShowOutput to use SVG entirely

--- mia-plugin/src/main/java/io/github/mianalysis/mia/gui/regions/workflowmodules/ShowOutputButton.java
+++ mia-plugin/src/main/java/io/github/mianalysis/mia/gui/regions/workflowmodules/ShowOutputButton.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Arrays;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -22,188 +23,153 @@ import com.github.weisj.jsvg.parser.SVGLoader;
 
 import io.github.mianalysis.mia.MIA;
 import io.github.mianalysis.mia.gui.GUI;
+import io.github.mianalysis.mia.gui.svg.CustomColorsProcessor;
+import io.github.mianalysis.mia.gui.svg.CustomParserProvider;
+import io.github.mianalysis.mia.gui.svg.DynamicAWTSvgPaint;
 import io.github.mianalysis.mia.module.Module;
+import io.github.mianalysis.mia.object.system.Colours;
 import io.github.mianalysis.mia.object.system.SwingPreferences;
 
 /**
  * Created by sc13967 on 07/06/2017.
  */
 public class ShowOutputButton extends JButton implements ActionListener, MouseListener {
-        /**
-         *
-         */
-        private static final int size = 18;
-        private Module module;
-        private boolean state = true;
-        private static final ImageIcon blackClosedIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_black_12px.png"), "");
-        private static final ImageIcon blackClosedIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_blackDM_12px.png"), "");
-        private static final ImageIcon blackOpenIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_black_12px.png"), "");
-        private static final ImageIcon blackOpenIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_blackDM_12px.png"), "");
-        private static final ImageIcon redClosedIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_red_12px.png"), "");
-        private static final ImageIcon redClosedIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_redDM_12px.png"), "");
-        private static final ImageIcon redOpenIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_red_12px.png"), "");
-        private static final ImageIcon redOpenIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_redDM_12px.png"), "");
-        private static final ImageIcon orangeClosedIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_orange_12px.png"), "");
-        private static final ImageIcon orangeClosedIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_orangeDM_12px.png"), "");
-        private static final ImageIcon orangeOpenIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_orange_12px.png"), "");
-        private static final ImageIcon orangeOpenIconDM = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_orangeDM_12px.png"), "");
-        private static final ImageIcon greyClosedIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeclosed_grey_12px.png"), "");
-        private static final ImageIcon greyOpenIcon = new ImageIcon(
-                        ShowOutputButton.class.getResource("/icons/eyeopen_grey_12px.png"), "");
+    /**
+     *
+     */
+    private static final int size = 18;
+    private Module module;
+    private boolean showOutput = true;
 
-        private static SVGLoader loader = new SVGLoader();
-        private static SVGDocument eyeSvgDoc = loader.load(ShowOutputButton.class.getResource("/icons/eyeopen.svg"));
-        private static SVGDocument eyeHoverSvgDoc = loader
-                        .load(ShowOutputButton.class.getResource("/icons/eyeopen_hover.svg"));
+    private SVGIcon eyeOpenIcon;
+    private SVGIcon eyeClosedIcon;
+    private DynamicAWTSvgPaint dynamicEyeColor;
+    private DynamicAWTSvgPaint dynamicGlowColor;
 
-        public ShowOutputButton(Module module) {
-                this.module = module;
+    public ShowOutputButton(Module module) {
+        this.module = module;
 
-                state = module.canShowOutput();
+        CustomColorsProcessor processor = new CustomColorsProcessor(Arrays.asList("glow", "foreground"));
+        CustomParserProvider provider = new CustomParserProvider(processor);
+        SVGDocument eyeOpenSvgDoc = new SVGLoader()
+                .load(ShowOutputButton.class.getResource("/icons/eyeopen.svg"), provider);
+        SVGDocument eyeClosedSvgDoc = new SVGLoader()
+                .load(ShowOutputButton.class.getResource("/icons/eyeclosed.svg"), provider);
 
-                putClientProperty(FlatClientProperties.STYLE, "arc: 0");
-                setBorderPainted(false);
-                addActionListener(this);
-                addMouseListener(this);
-                setFocusPainted(false);
-                setOpaque(false);
-                setBackground(new Color(0, 0, 0, 0));
-                setSelected(false);
-                setMargin(new Insets(0, 0, 0, 0));
-                setName("Show output");
-                setToolTipText("Show output from module");
-                updateState();
+        eyeOpenIcon = new SVGIcon(eyeOpenSvgDoc, size, size);
+        eyeClosedIcon = new SVGIcon(eyeClosedSvgDoc, size, size);
+        dynamicEyeColor = processor.customColorForId("foreground");
+        dynamicGlowColor = processor.customColorForId("glow");
 
+        dynamicGlowColor.setColor(new Color(0, 0, 0, 0));
+
+        showOutput = module.canShowOutput();
+
+        putClientProperty(FlatClientProperties.STYLE, "arc: 0");
+        setBorderPainted(false);
+        addActionListener(this);
+        addMouseListener(this);
+        setFocusPainted(false);
+        setOpaque(false);
+        setBackground(new Color(0, 0, 0, 0));
+        setSelected(false);
+        setMargin(new Insets(0, 0, 0, 0));
+        setName("Show output");
+        setToolTipText("Show output from module");
+
+        updateState();
+
+    }
+
+    public void updateState() {
+        boolean isDark = ((SwingPreferences) MIA.getPreferences()).darkThemeEnabled();
+
+        if (showOutput)
+            setIcon(eyeOpenIcon);
+        else
+            setIcon(eyeClosedIcon);
+
+        if ((module.isEnabled()) && module.isReachable() &&
+                module.isRunnable()) {
+            dynamicEyeColor.setColor(Colours.getDarkGrey(isDark));
+        } else if ((module.isEnabled()) & !module.isReachable()) {
+            dynamicEyeColor.setColor(Colours.getOrange(isDark));
+        } else if ((module.isEnabled()) & !module.isRunnable()) {
+            dynamicEyeColor.setColor(Colours.getRed(isDark));
+        } else if (!module.isEnabled()) {
+            dynamicEyeColor.setColor(Color.GRAY);
         }
+    }
 
-        public void updateState() {
-                boolean isDark = ((SwingPreferences) MIA.getPreferences()).darkThemeEnabled();
+    public Module getModule() {
+        return module;
+    }
 
-                // if ((state && module.isEnabled()) && module.isReachable() &&
-                // module.isRunnable()) {
-                // if (isDark)
-                // setIcon(blackOpenIconDM);
-                // else
-                // setIcon(blackOpenIcon);
-                // } else if ((state && module.isEnabled()) & !module.isReachable()) {
-                // if (isDark)
-                // setIcon(orangeOpenIconDM);
-                // else
-                // setIcon(orangeOpenIcon);
-                // } else if ((state && module.isEnabled()) & !module.isRunnable()) {
-                // if (isDark)
-                // setIcon(redOpenIconDM);
-                // else
-                // setIcon(redOpenIcon);
-                // } else if (state & !module.isEnabled()) {
-                // setIcon(greyOpenIcon);
-                // } else if ((!state && module.isEnabled()) && module.isReachable() &&
-                // module.isRunnable()) {
-                // if (isDark)
-                // setIcon(blackClosedIconDM);
-                // else
-                // setIcon(blackClosedIcon);
-                // } else if ((!state && module.isEnabled()) & !module.isReachable()) {
-                // if (isDark)
-                // setIcon(orangeClosedIconDM);
-                // else
-                // setIcon(orangeClosedIcon);
-                // } else if ((!state && module.isEnabled()) & !module.isRunnable()) {
-                // if (isDark)
-                // setIcon(redClosedIconDM);
-                // else
-                // setIcon(redClosedIcon);
-                // } else if (!state & !module.isEnabled()) {
-                // setIcon(greyClosedIcon);
-                // }
-                // setIcon(generateIcon());
-                setIcon(new SVGIcon(eyeSvgDoc, size, size));
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        GUI.addUndo();
 
-        }
+        // Invert state
+        showOutput = !showOutput;
 
-        public Module getModule() {
-                return module;
-        }
+        updateState();
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-                GUI.addUndo();
+        module.setShowOutput(showOutput);
 
-                // Invert state
-                state = !state;
+    }
 
-                updateState();
-                module.setShowOutput(state);
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
 
-        }
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
 
-        @Override
-        public void mouseClicked(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
-        }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
 
-        @Override
-        public void mousePressed(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
-        }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        dynamicGlowColor.setColor(Colours.getDarkBlue(false));
+        repaint();
 
-        @Override
-        public void mouseReleased(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
-        }
+    }
 
-        @Override
-        public void mouseEntered(MouseEvent e) {
-                setIcon(new SVGIcon(eyeHoverSvgDoc, size, size));
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-                setIcon(new SVGIcon(eyeSvgDoc, size, size));
-        }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        dynamicGlowColor.setColor(new Color(0, 0, 0, 0));
+        repaint();
+    }
 }
 
 class SVGIcon implements Icon {
-        private final SVGDocument document;
-        private final int width;
-        private final int height;
+    private final SVGDocument document;
+    private final int width;
+    private final int height;
 
-        public SVGIcon(SVGDocument document, int width, int height) {
-                this.document = document;
-                this.width = width;
-                this.height = height;
-        }
+    public SVGIcon(SVGDocument document, int width, int height) {
+        this.document = document;
+        this.width = width;
+        this.height = height;
 
-        @Override
-        public void paintIcon(Component c, Graphics g, int x, int y) {
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                document.render(c, g2d, new ViewBox(x, y, width, height));
-        }
+    }
 
-        @Override
-        public int getIconWidth() {
-                return width;
-        }
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        document.render(c, g2d, new ViewBox(x, y, width, height));
+    }
 
-        @Override
-        public int getIconHeight() {
-                return height;
-        }
+    @Override
+    public int getIconWidth() {
+        return width;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return height;
+    }
 }
